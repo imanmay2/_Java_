a0409e3e446b6230fd74b4f4d7a27281cd7eeba5
Manmay <manmaychakarborty@gmail.com>
2022-03-23 22:13:37 +0530

    updated version but bug yet to be fixed....

--- happy_number.java
+++ happy_number.java
@@ -2,37 +2,37 @@
 import java.util.*;
 public class happy_number{
     public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int n;
-    System.out.println("Enter the number to check wheather it is a happy number or a sad number");
-    n=sc.nextInt();
-    int p=0;
-    if(n>0 && n<10){
-        System.out.println(n+" is a sad number");
-    }
-    else{
-        while(true){
-            int s=0;
-            while(n>0){
-                int r=n%10;
-                s+=(r*r);
-                n=n/10;
-            }
-            if(s<10){
-                p=s;
-                break;
-            }
-            else{
-                n=s;
-            }
-
-        }
-        if(p==1){
-            System.out.println(n," is a happy number");
-        }
-        else{
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number to check wheather it is a happy number or a sad number");
+        n=sc.nextInt();
+        int p=0;
+        if(n>0 && n<10){
             System.out.println(n+" is a sad number");
         }
+        else{
+            while(true){
+                int s=0;
+                while(n>0){
+                    int r=n%10;
+                    s+=(r*r);
+                    n=n/10;
+                }
+                if(s<10){
+                    p=s;
+                    break;
+                }
+                else{
+                    n=s;
+                }
+
+            }
+            if(p==1){
+                System.out.println(n," is a happy number");
+            }
+            else{
+                System.out.println(n+" is a sad number");
+            }
+        }
     }
-}
 }
