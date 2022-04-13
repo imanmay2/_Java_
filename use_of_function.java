ffb54ad9278370d0bd01399bf9767bd9a7232b40
Manmay <manmaychakarborty@gmail.com>
2022-04-13 09:09:14 +0530

    updated

--- use_of_function.java
+++ use_of_function.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class use_of_function{
     public static void printJava(){
-        System.out.print("Hello buddy!!Here is my first function program in JAVA");
+        System.out.println("Hello buddy!!Here is my first function program in JAVA");
     }
     public static void main(String[] args){
         {
