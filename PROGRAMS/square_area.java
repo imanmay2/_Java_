511190dc966f4549916fd65995532ea93d4c4a3c
manmay2 <imanmay2@gmail.com>
2024-05-19 06:43:44 +0530

    ASSisgnment 1

---
+++ PROGRAMS/square_area.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class square_area{
+    public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the side of the square : ");
+    float s=sc.nextFloat();
+    float ar=s*s;
+    System.out.println("Area of the square is : "+ar);
+}}
