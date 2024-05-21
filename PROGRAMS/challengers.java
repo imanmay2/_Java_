7d0ee274304da7295cb2b0989eec76c50a676dc5
manmay2 <imanmay2@gmail.com>
2024-05-21 20:28:15 +0530

    CHANGELLERS

---
+++ PROGRAMS/challengers.java
@@ -0,0 +1,82 @@
+ // 1. Print a matrix in a spiral order.
+ 
+ // 2. Given an array A[] consisting of only 0s, 1s, and 2s. 
+ //        The task is to write a function that sorts the given array. 
+ //        The functions should put all 0s first, then all 1s and all 2s in last.
+ // 3. Move all the -ve elements on the left side of the array.
+
+ //4. Assign Roll Number to student in alphabetical order.
+ /* 5 .Find a peak element which is not smaller than its neighbours
+Given an array arr of n elements that is first strictly increasing and then maybe strictly decreasing, find the maximum element in the array.
+
+Note: If the array is increasing then just print the last element will be the maximum value.
+
+Example:
+
+Input: array[]= {5, 10, 20, 15}
+Output: 20
+Explanation: The element 20 has neighbors 10 and 15, both of them are less than 20. */
+
+
+
+
+//6. Smallest positive missing number . 
+//Accoute,Amazon,Samsung,Snapdeal
+
+//You are given an array arr[] of N integres including 0. the task is to find the smallest
+//positive number missing from the array.
+//e.g : [0,-9,1,3,-4,5]
+//Hence, the first positive missing number is 2.
+
+
+
+// 7 . Implement Stack Operation in Java.
+
+
+//8. Find union and intersection of 2 arrays.
+
+
+//9.// WAP that will display the count of the most repeating string and the string too.
+
+
+//10.// Write a program that will find the pair of the given sum in the ARAAY.
+
+
+////11. Given an array arr[] of size N. The task is to find the first repeating element in the array of integres. i.e an
+//element that occurs more than once and whose index of the first occurence is smallest.
+//	E.g : [1,5,3,4,3,5,6]
+//	 output : 2               (FACEBOOK , AMAZON )
+
+
+
+
+// 12. //WAP THAT WILL ACCEPT A SENTENCE AND PRINT THE LARGEST WORD FROM THAT SENTENCE/.
+
+//13.  WAP that will create an array and also accept an element
+   // and find whether it is present or not in the array, if present return the index value.
+
+
+//14. // longest arithmetic subarray.
+// e.g : [10,7,4,6,8,10,11]
+//       sub-array :[2,4,6,8] 
+//       output should be 4.
+// exclude the condition for 0.(it means , if the difference between the number is 0 , then skip that).
+
+
+
+
+
+//15. // REcord breaking data. 
+// [1,2,0,7,2,0,2,2]
+// conditions to be noted is that : 
+// the number of visitors on that day is strictly larger than the number 
+// of visitors on each of the previous days.
+// Either it is the last day, or the number of visitors on the day is strictly larger than the number of visitors 
+// on the following day.
+// NOTE : every first day could be record breaking days.
+
+
+
+
+
+//16. // Given an array a[] of size n. Output sum of each subarray of the given array.
