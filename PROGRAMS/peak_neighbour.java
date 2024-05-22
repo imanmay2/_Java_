68a24f96d801b0d2c4691b740bc019ee4b56463e
manmay2 <imanmay2@gmail.com>
2024-05-22 22:19:13 +0530

    Done with 1st DSA Q

--- PROGRAMS/peak_neighbour.java
+++ PROGRAMS/peak_neighbour.java
@@ -17,6 +17,25 @@ public class peak_neighbour{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the length of the array : ");
         int n=sc.nextInt();
-        
+        int[] arr=new int[n];   // create an array of size n;
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the element  : ");
+            arr[i]=sc.nextInt();
+        }
+
+
+        // main algo starts from here.    [1,2,3,4]
+        for(int i=0;i<=n-1;i++){
+            if(i==0 && arr[i]>arr[i+1]){
+                System.out.println("Peak Element is : "+arr[i]);
+            }
+            else if(i==n-1 && arr[i]>arr[i-1]){
+                System.out.println("Peak Element is : "+arr[i]);
+            }
+            else if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+                System.out.println("Peak Element is : "+arr[i]);
+            }
+            
+        }
     }
 }
