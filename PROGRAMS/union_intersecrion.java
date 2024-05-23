79760b6d7854ca769ed262f9fc6c572f4750a63a
manmay2 <imanmay2@gmail.com>
2024-05-23 08:24:18 +0530

    logic yet to be deployed

---
+++ PROGRAMS/union_intersecrion.java
@@ -0,0 +1,28 @@
+// Find union and intersection of 2 arrays.
+
+
+import java.util.*;
+public class union_intersection{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the size of the arrays : ");
+        int n=sc.nextInt();
+        int[] arr1=new int[n];
+        //// inserting elements in the 1st array.
+        for(int i=0;i<n;i++){
+            System.out.println("Enter element for array1: ");
+            arr1[i]=nextInt();
+        }
+        int[] arr2=new int[n];
+        //// inserting elements in the 2nd array.
+        for(int j=0;j<n;j++){
+            System.out.println("Enter the element for array2 : ");
+            arr2[i]=sc.nextInt();
+        }
+
+
+
+        // main algo start here...
+      
+            }
+        }
