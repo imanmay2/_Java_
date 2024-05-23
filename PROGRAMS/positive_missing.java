b2a38cc5611adba2dd9290e262a01c5aa1cb5dff
manmay2 <imanmay2@gmail.com>
2024-05-23 08:02:52 +0530

    logic yet to be deployed

--- PROGRAMS/positive_missing.java
+++ PROGRAMS/positive_missing.java
@@ -6,3 +6,31 @@
 //Hence, the first positive missing number is 2.
 
 
+import java.util.*;
+public class positive_missing{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the element  : ");
+            arr[i]=sc.nextInt();
+        }
+        //main algo starts from here.
+        boolean f=false;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=0){
+                for(int j=i+1;j<n;j++){
+                    if(arr[i]+1==arr[j]){
+                        continue;
+                }
+
+            }
+        }
+    }
+    if(f==false){
+        System.out.println("Sorry !! Small positve missing number not found !!");
+    }
+}
+}
