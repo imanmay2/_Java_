366e2d3ef4fd1f423cc0ecd3009067bc496e0e4f
manmay2 <imanmay2@gmail.com>
2024-05-28 20:37:24 +0530

    bottom left trianagle pattern done

---
+++ PROGRAMS/bottom_left_TRI_pattern.java
@@ -0,0 +1,13 @@
+// bottom left triangle pattern
+
+
+public class bottom_left_TRI_pattern{
+    public static void main(String args[]){
+        for(int i=1;i<6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
