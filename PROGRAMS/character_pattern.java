da5f79e4e663021f9a504898965df24f44039e1c
manmay2 <imanmay2@gmail.com>
2024-05-28 09:09:58 +0530

    character pattern done

---
+++ PROGRAMS/character_pattern.java
@@ -0,0 +1,15 @@
+// Character pattern.
+
+
+public class character_pattern{
+    public static void main(String args[]){
+        int k=65;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)k);
+                k++;
+            }
+            System.out.println("");
+        }
+    }
+}
