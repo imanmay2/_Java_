92924606e640c6000d4c058a375ea18da4a28551
manmay2 <imanmay2@gmail.com>
2024-05-30 12:02:04 +0530

    logic yet to be deployed

--- PROGRAMS/positive_missing.java
+++ PROGRAMS/positive_missing.java
@@ -1,9 +1,9 @@
 // Smallest positive missing number .
-//Accoute,Amazon,Samsung,Snapdeal
-//You are given an array arr[] of N integres including 0. the task is to find the smallest
-//positive number missing from the array.
-//e.g : [0,-9,1,3,-4,5]
-//Hence, the first positive missing number is 2.
+// Accoute,Amazon,Samsung,Snapdeal
+// You are given an array arr[] of N integres including 0. the task is to find the smallest
+// positive number missing from the array.
+// e.g : [0,-9,1,3,-4,5]
+// Hence, the first positive missing number is 2.
 
 
 import java.util.*;
@@ -18,27 +18,7 @@ public class positive_missing{
             arr[i]=sc.nextInt();
         }
         //main algo starts from here.
-        boolean f=false;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                // if(arr[i]>arr[j]){
-                //     continue;
-                // }
 
-            if((arr[i]<arr[j]) && (arr[j]-arr[i])>1){
-                System.out.println("Small Positive missing number is : "+(arr[i]+1));
-                f=true;
-                break;
-            }
         
-
-        }
-        if(f==true){
-            break;
-        }
     }
-    if(f==false){
-        System.out.println("Sorry !! Small positve missing number not found !!");
-    }
-}
 }
