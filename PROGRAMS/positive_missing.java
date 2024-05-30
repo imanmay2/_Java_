0ed0fc575cad9563419970e0d2082ce229f2bd44
manmay2 <imanmay2@gmail.com>
2024-05-30 12:17:29 +0530

    solved small positive missing number

--- PROGRAMS/positive_missing.java
+++ PROGRAMS/positive_missing.java
@@ -8,6 +8,16 @@
 
 import java.util.*;
 public class positive_missing{
+    public static boolean check(int n,int arr[]){
+        for(int i=0;i<arr.length;i++){
+            if(n==arr[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of the array : ");
@@ -18,7 +28,16 @@ public class positive_missing{
             arr[i]=sc.nextInt();
         }
         //main algo starts from here.
+        int p=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=0){
+                p=arr[i];
+            }
+            if(check((p+1),arr)==false){
+                System.out.println("Smallest missing number is : "+(p+1));
+                break;
+            }
 
-        
+        }
     }
 }
