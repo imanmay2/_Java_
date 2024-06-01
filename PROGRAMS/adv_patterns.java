8445be72ca85befa45e0eb208b79275405316ad1
manmay2 <imanmay2@gmail.com>
2024-06-01 10:10:48 +0530

    X_pattern Added

--- PROGRAMS/adv_patterns.java
+++ PROGRAMS/adv_patterns.java
@@ -156,7 +156,22 @@ public class adv_patterns{
     }
 
 
+    public static void X_pattern(){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if((i==j) || (i+j)==6){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void main(String args[]){
-        
+        X_pattern();
     }
 }
