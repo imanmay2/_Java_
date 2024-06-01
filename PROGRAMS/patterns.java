87c02a35d2b803528b2ebb2143154c38a3f35657
manmay2 <imanmay2@gmail.com>
2024-06-01 09:50:42 +0530

    diamond updated

--- PROGRAMS/patterns.java
+++ PROGRAMS/patterns.java
@@ -66,9 +66,97 @@ public class patterns{
     }
 
 
+    public static void butterfly(int n){
+        //Printing the patterrn for the first half....
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+
+        //printing the 2nd half of the pattern.
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void rhombus(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=n;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void hollow_rhombus(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void diamond(int n){
+
+        //printing for 1st half
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        //printing for 2nd half
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
 
     public static void main(String args[]){
-        inverted_half_pyra();
+        diamond(4);
     }
 }
