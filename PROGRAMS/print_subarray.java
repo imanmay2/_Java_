819d47bb3f46edfd3fa9dd84bf64a794b6686fff
manmay2 <imanmay2@gmail.com>
2024-06-04 17:34:40 +0530

    logic yet to be deployed

---
+++ PROGRAMS/print_subarray.java
@@ -0,0 +1,13 @@
+// Print the subarrays of the given array.
+
+
+public class print_subarray{
+    public static void main(String args[]){
+        int[] arr={1,2,3,4,5,6};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++)[
+                System.out.println();
+            ]
+        }
+    }
+}
