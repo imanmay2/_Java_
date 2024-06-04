0af089ff1b978e7bb6c3c6a09d04e6a4316891c0
manmay2 <imanmay2@gmail.com>
2024-06-04 17:48:34 +0530

    subarray code done

--- PROGRAMS/print_subarray.java
+++ PROGRAMS/print_subarray.java
@@ -4,10 +4,15 @@
 public class print_subarray{
     public static void main(String args[]){
         int[] arr={1,2,3,4,5,6};
+        System.out.println("The subarrays are : ");
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++)[
-                System.out.println();
-            ]
+            for(int j=i;j<arr.length;j++){
+                System.out.print(+arr[j]);
+                if(j!=(arr.length-1)){
+                    System.out.print(",");
+                }
+            }
+            System.out.println();
         }
     }
 }
