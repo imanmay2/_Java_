6b50ab1fad3cc2d62a0d071a56716bdfc109e4d6
manmay2 <imanmay2@gmail.com>
2024-06-04 08:08:46 +0530

    trial tested

---
+++ PROGRAMS/trial.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class trial{
+    public static void main(String args[]){
+        int[] arr=new int[8];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        System.out.println(arr[3]);
+    }
+}
