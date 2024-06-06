cc71fcce3713b2316bca14d78b4af4529bdc4e0b
manmay2 <imanmay2@gmail.com>
2024-06-06 07:43:46 +0530

    done with maximum sum subarray

---
+++ PROGRAMS/max_sum_subarray.java
@@ -0,0 +1,25 @@
+// WAP that will  return the maximum sum of the subarray.
+
+
+import java.util.*;
+public class max_sum_subarray{
+    public static int max_sum(int[] arr){
+        int sum=0,s=0;
+        for(int i=0;i<arr.length;i++){
+            s=0;
+            for(int j=i;j<arr.length;++j){
+                s=s+arr[j];
+            }
+            if(s>sum){
+                sum=s;
+            }
+        }
+        return sum;
+    }
+
+
+    public static void main(String args[]){
+        int[] arr={2,4,6,8,10};
+        System.out.println("The maximum value of the subarray is: "+max_sum(arr));
+    }
+}
