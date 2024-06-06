b14167106a788d595d00040031ae54ba56ea7e74
manmay2 <imanmay2@gmail.com>
2024-06-06 08:06:16 +0530

    prefix sum method updated

--- PROGRAMS/max_sum_subarray.java
+++ PROGRAMS/max_sum_subarray.java
@@ -23,3 +23,27 @@ public class max_sum_subarray{
         System.out.println("The maximum value of the subarray is: "+max_sum(arr));
     }
 }
+
+
+
+                            // Prefix Sum method to calculate the max sum of the subarray.
+
+import java.util.*;
+public class max_sum_subarray{
+    public static int max_sum(int[] arr){
+        int sum=0,s=0;
+        int[] pre_arr=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            s=s+arr[i];
+            pre_arr[i]=s;
+        }
+        return pre_arr[(arr.length-1)];
+    }
+
+
+
+    public static void main(String args[]){
+        int[] arr={2,4,6,8,10};
+        System.out.println("The max sum of the array : "+max_sum(arr));
+    }
+}
