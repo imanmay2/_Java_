98932929909893b3aafac4d2845ac38fa8879b8a
manmay2 <imanmay2@gmail.com>
2024-06-08 06:49:59 +0530

    bubble sorting done

--- PROGRAMS/trial.java
+++ PROGRAMS/trial.java
@@ -1,7 +1,11 @@
 import java.util.*;
 public class trial{
     public static void main(String args[]){
-        int[] arr={1,2,3,4};
-        System.out.print(arr.max());
+        int a=2;
+        int b=4;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.print(a+","+b);
     }
 }
