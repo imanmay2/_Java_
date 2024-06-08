15b2adc85d98d59930ed8ffe97fd05eeae80c48e
manmay2 <imanmay2@gmail.com>
2024-06-08 07:15:08 +0530

    insertion sort

--- PROGRAMS/SORTING.java
+++ PROGRAMS/SORTING.java
@@ -31,11 +31,26 @@ public class SORTING{
     }
 
 
+    public static void insertion_sort(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+
+            //insertion
+            arr[prev+1]=curr;
+        }
+    }
+
+
     public static void main(String args[]){
         int[] arr={8,1,7,2,6,3,5};
-        selection_sort(arr);
+        insertion_sort(arr);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
-    }
+    } 
 }
