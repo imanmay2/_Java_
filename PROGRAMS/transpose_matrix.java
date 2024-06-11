27b44e7da8a4e79d66c8c7db539310e769b665c9
manmay2 <imanmay2@gmail.com>
2024-06-11 13:28:20 +0530

    Assignment #Q3. Done

--- PROGRAMS/transpose_matrix.java
+++ PROGRAMS/transpose_matrix.java
@@ -4,23 +4,29 @@
 import java.util.*;
 public class transpose_matrix{
     public static void main(String args[]){
-        int[][]={{1,2,3},{4,5,6}};
-        //printing the matrix.
+        int[][] arr={
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12}
+        };
+        
+
+        int[][] transpose=new int[arr[0].length][arr.length];
         for(int i=0;i<arr.length;i++){
-            for(j=0;j<arr[0].length;j++){
-                System.out.print(arr[i][j]+" ");
+            for(int j=0;j<arr[0].length;j++){
+                transpose[j][i]=arr[i][j];
+            }
+        }
+
+
+
+        //printing the transpose array.
+        for(int i=0;i<transpose.length;i++){
+            for(int j=0;j<transpose[0].length;j++){
+                System.out.print(transpose[i][j]+" ");
             }
             System.out.println();
         }
-
-
-
-        //transposing the matrix.
-        int[][]=new int[arr[0].length-1][];
-        for(int i=0;i<arr[0].length;i++){
-            for(int j=0;j<arr.length;j++){
-                transpose[i][j]=
-            }
-        }
+        
     }
 }
