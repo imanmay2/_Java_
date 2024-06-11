dd03ec176433d0383cd1f09438c3c4cdd48b64df
manmay2 <imanmay2@gmail.com>
2024-06-11 12:03:42 +0530

    updated

---
+++ PROGRAMS/transpose_matrix.java
@@ -0,0 +1,26 @@
+// WAP that will print the transpose of a 2D matrix.
+
+
+import java.util.*;
+public class transpose_matrix{
+    public static void main(String args[]){
+        int[][]={{1,2,3},{4,5,6}};
+        //printing the matrix.
+        for(int i=0;i<arr.length;i++){
+            for(j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+
+        //transposing the matrix.
+        int[][]=new int[arr[0].length-1][];
+        for(int i=0;i<arr[0].length;i++){
+            for(int j=0;j<arr.length;j++){
+                transpose[i][j]=
+            }
+        }
+    }
+}
