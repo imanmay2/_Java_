0f5183a217d8457bd5389e29288d0e988faf915e
manmay2 <imanmay2@gmail.com>
2024-06-13 18:57:01 +0530

    logic yet to be done

---
+++ PROGRAMS/anagrams_str.java
@@ -0,0 +1,10 @@
+// Determine if 2 Strings are anagrams of each other.
+// What are anagrams?
+// If two strings contain the same characters but in a different order, 
+//they can be said to be anagrams. Consider race and care. 
+//In this case, race's characters can be formed into a study, 
+//or care's characters can be formed into race. Below is a java program to check if two strings are anagrams or not.
+
+
+
+
--- PROGRAMS/trial.java
+++ PROGRAMS/trial.java
@@ -1,11 +1,6 @@
-import java.util.*;
-public class trial{
-    public static void main(String args[]){
-        int a=2;
-        int b=4;
-        a=a+b;
-        b=a-b;
-        a=a-b;
-        System.out.print(a+","+b);
-    }
+public class trial {
+ public static void main(String args[]) {
+   String str = "ApnaCollege".replace("l", "");
+   System.out.println(str);
+ }
 }
