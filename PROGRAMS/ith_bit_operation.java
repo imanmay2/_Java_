ecec7f16b4b29710013d96fc555638cb03308376
manmay2 <imanmay2@gmail.com>
2024-06-18 11:37:52 +0530

    updated to ith bit, dynamically

--- PROGRAMS/ith_bit_operation.java
+++ PROGRAMS/ith_bit_operation.java
@@ -14,9 +14,17 @@ public class ith_bit_operation{
     public static void clear_bit(int i,int n){
         System.out.println("The updated bit is : "+ (~(1<<i)&n));
     }
+    public static void update_ith_bit(int i,int n,int u){
+        if(u==0){
+            clear_bit(i,n);
+        }
+        else if(u==1){
+            set_bit(i,n);
+        }
+    }
 
 
     public static void main(String args[]){
-        clear_bit(1,10);
+        update_ith_bit(1,10,1);
     }
 }
