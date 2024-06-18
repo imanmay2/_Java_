388ee0101ec66fb3a9353e1913259842105a98d1
manmay2 <imanmay2@gmail.com>
2024-06-18 10:29:28 +0530

    bit's manipulation pro

---
+++ PROGRAMS/ith_bit_operation.java
@@ -0,0 +1,22 @@
+// get,set,clear ith bith operations...
+
+
+
+public class ith_bit_operation{
+    public static  void get_bit(int i,int n){
+        System.out.println("The ith bit is : "+ (1<<i)&n);
+    }
+
+    public static void set_bit(int i,int n){
+        System.out.println("The updated bit is"+ (1<<i)||n);
+    }
+
+    public static void clear_bit(int i,int n){
+        System.out.println("The updated bit is : "+ (~(1<<i)&n));
+    }
+
+
+    public static void main(String args[]){
+        get_bit();
+    }
+}
