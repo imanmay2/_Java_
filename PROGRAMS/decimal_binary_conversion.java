9712129eed016362bf48d5a5de65766c414389a9
manmay2 <imanmay2@gmail.com>
2024-06-18 11:01:20 +0530

    yet to be done

---
+++ PROGRAMS/decimal_binary_conversion.java
@@ -0,0 +1,21 @@
+// Decimal to binary and vice versa conversion...
+import java.util.*;
+public class decimal_binary_conversion{
+    public static void decimal_binary(int n){
+        StringBuilder str=new StringBuilder("");
+        while(n!=0){
+            int r=n%2;
+            str.append(r);
+            
+
+        }
+    }
+
+
+
+
+    public static void main(String args[]){
+        decimal_binary();
+        binary_decimal();
+    }
+}
