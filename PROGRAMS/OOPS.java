5b6ad7fb79b28dcdf1912222afcaec99190dde99
manmay2 <imanmay2@gmail.com>
2024-06-21 09:39:21 +0530

    Access Modifier learnt..

--- PROGRAMS/OOPS.java
+++ PROGRAMS/OOPS.java
@@ -5,6 +5,16 @@ public class OOPS{
         System.out.println(p1.color);   //properties in class.
         p1.setTip(8);        
         System.out.println(p1.tip);
+
+
+
+        //Creating bank account Class.
+        Bank_Account myAcc=new Bank_Account();
+        myAcc.username="imanmay2";
+        System.out.println(myAcc.username);
+        // System.out.println(myAcc.password);      // this has private access,  we can't acess it directly...
+
+        myAcc.update_Password("!@#$");
     }
 }
 
@@ -18,5 +28,15 @@ class Pen{
 
     void setTip(int tip_){
         tip=tip_;
+    }
+}
+
+
+class Bank_Account{
+    public String username;
+    private String password;
+    public void update_Password(String passwd){
+        passwd=passwd;
+        System.out.println("Password updated");
     }
 }
