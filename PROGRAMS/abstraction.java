2079778e11dac0ab90c4e9b958ee4a6bc462b3af
manmay2 <imanmay2@gmail.com>
2024-06-22 10:28:48 +0530

    Abstraction is walking lol

---
+++ PROGRAMS/abstraction.java
@@ -0,0 +1,28 @@
+public class abstraction{
+    public static void main(String args[]){
+        Horse h1=new Horse();
+        h1.walk();
+        Chicken c1=new Chicken();
+        c1.walk();
+    }
+}
+
+
+abstract class Animal{
+    void eat(){
+        System.out.println("Animal eats!!");
+    }
+    abstract void walk();
+}
+
+class Horse extends Animal{
+    void walk(){
+        System.out.println("Horse walks");
+    }
+}
+
+class Chicken extends Animal{
+    void walk(){
+        System.out.println("Chicken walks");
+    }
+}
