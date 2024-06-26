0e47ef3a15b6fdd064934c16128a492b759f0495
manmay2 <imanmay2@gmail.com>
2024-06-26 12:57:36 +0530

    yet to be done

---
+++ PROGRAMS/factorial_recursion.java
@@ -0,0 +1,10 @@
+public class factorial_recursion{
+    public static int fact(int num){
+        if(num==1){
+            return 1;
+        }
+        
+
+    }
+    public static void main(String args[]){}
+}
