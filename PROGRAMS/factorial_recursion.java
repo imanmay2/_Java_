3b1563b3e80badc6ddccb675be03256c31e83cfb
manmay2 <imanmay2@gmail.com>
2024-06-26 16:52:05 +0530

    factorial using recursion

--- PROGRAMS/factorial_recursion.java
+++ PROGRAMS/factorial_recursion.java
@@ -1,10 +1,17 @@
+import java.util.*;
 public class factorial_recursion{
     public static int fact(int num){
-        if(num==1){
+        int f=1;
+        if(num==0){
             return 1;
         }
-        
-
+        f=num*fact(num-1);
+        return f;
     }
-    public static void main(String args[]){}
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.print("Factorial of the number is : ");
+        System.out.print(fact(num));
+    }
 }
