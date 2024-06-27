637260fe40da3601d6fb478d7d3b1e013bd82a0f
manmay2 <imanmay2@gmail.com>
2024-06-27 10:37:01 +0530

    logic yet to be deployed

---
+++ PROGRAMS/last_occurence.java
@@ -0,0 +1,9 @@
+// Write a recursive function in java....that prints the index of the last occurence of the key...
+
+
+
+public class last_occurence{
+    public static int check(int arr[],int key,int i){
+        
+    }
+}
