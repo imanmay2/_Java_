201a7fdf8ea142794f323d7c0ec92a9b815c1e03
manmay2 <imanmay2@gmail.com>
2024-06-27 07:56:39 +0530

    code updated

--- PROGRAMS/fibonacci_recursion.java
+++ PROGRAMS/fibonacci_recursion.java
@@ -5,12 +5,10 @@ import java.util.*;
 public class fibonacci_recursion{
     public static int fibo(int n){
         int f=0;
-        if(n==0){
-            return 0;
+        if(n==0 || n==1){
+            return n;
         }
-        else if(n==1){
-            return 1;
-        }
+        
 
         f=fibo(n-1)+fibo(n-2);
         return f;
