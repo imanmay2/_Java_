92b5832c0fa948a75a1d9b4365887b9ecd4c7036
manmay2 <imanmay2@gmail.com>
2024-06-28 11:18:20 +0530

    donw using recursion as well

--- PROGRAMS/remove_duplicate_string.java
+++ PROGRAMS/remove_duplicate_string.java
@@ -20,8 +20,27 @@ public class remove_duplicate_string{
     }
 
 
+    public static String remove_recursion(String str,int i,boolean arr[],StringBuilder sb){
+        char chr;
+        if(i==str.length()){
+            return sb.toString();
+        }
+        chr=str.charAt(i);
+        int char_arr=chr-'a';
+        if(arr[char_arr]==false){
+            sb.append(chr);
+            arr[char_arr]=true;
+        }
+
+        return remove_recursion(str,i+1,arr,sb);
+
+    }
+
+
 
     public static void main(String args[]){
-        System.out.print(remove("appnacollege"));
+        StringBuilder sb=new StringBuilder("");
+        boolean[] arr=new boolean[26];
+        System.out.print(remove_recursion("appnacollege",0,arr,sb));
     }
 }
