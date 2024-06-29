4a30a8c19adf28e173b48af12525ed7414a83303
manmay2 <imanmay2@gmail.com>
2024-06-29 14:15:51 +0530

    Assignement Q 3 of recursion done

--- PROGRAMS/trial.java
+++ PROGRAMS/trial.java
@@ -1,6 +1,6 @@
 public class trial{
     public static void main(String args[]){
         String str="2019";
-        System.out.print(str.charAt(0)-'0');
+        System.out.print(str.substring(1));
     }
 }
