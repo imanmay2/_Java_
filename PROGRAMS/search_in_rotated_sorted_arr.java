816c3fb34daaf2883c3ea11b8400559ed7f99fd7
manmay2 <imanmay2@gmail.com>
2024-07-01 20:29:34 +0530

    done , serch in rotated sorted array....

--- PROGRAMS/search_in_rotated_sorted_arr.java
+++ PROGRAMS/search_in_rotated_sorted_arr.java
@@ -4,23 +4,34 @@
 public class search_in_rotated_sorted_arr{
     public static int search(int arr[],int si,int target,int ei){
         int mid=(si+ei)/2;
-        if(arr[mid]==target){
+        // basecase 
+        if(si>ei){
+            return -1;
+        }
+        else if(arr[mid]==target){
             return mid;
         }
-        else if(target>arr[mid]){
-            return search(arr,mid+1,target,ei);
+        else if(arr[si]<=arr[mid]){
+            if(arr[si]<=target && target<=arr[mid]){
+                return search(arr,si,target,mid);
+            }
+            else{
+                return search(arr,mid+1,target,ei);
+            }
+
         }
-        else if(target<arr[mid]){
-            return search(arr,si,target,mid-1);
-        }
-        else if(si>ei){
-            System.out.println(si+" "+ ei);
-            return -1;
+        else if(arr[mid]<=arr[ei]){
+            if(target<=arr[ei] && target>=arr[mid]){
+                return search(arr,mid,target,ei);
+            }
+            else{
+                return search(arr,si,target,mid-1);
+            }
         }
         return 0;
     }
     public static void main(String args[]){
-        int target=4;
+        int target=2;
         int arr[]={4,5,6,7,0,1,2};
         System.out.println(search(arr,0,target,arr.length-1));
     }
