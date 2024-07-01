664d1f6c60419f9204a0218089ae6b3d6ec3a1a2
manmay2 <imanmay2@gmail.com>
2024-07-01 08:24:03 +0530

    bug yet to be fixed

---
+++ PROGRAMS/quick_sort.java
@@ -0,0 +1,47 @@
+// Write a program that will sort an array using quick sort algorithm...
+
+
+public class quick_sort{
+    public static void quick_sort(int arr[],int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int pIdx=partition(arr,si,ei);
+        quick_sort(arr,si,pIdx-1);
+        quick_sort(arr,pIdx+1,ei);
+    }
+
+
+    public static int partition(int arr[],int si,int ei);{
+        int i=-1;
+        int pivot=arr[ei];
+        for(int j=si;j<ei;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+        int temp=pivot;
+        arr[ei]=arr[i];
+        arr[i]=temp;
+        return i;
+
+    }
+
+
+    public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+    public static void main(String args[]){
+        int arr[]={6,3,9,8,2,5};
+        quick_sort(arr,0,arr.length-1);
+        print(arr);
+    }
+}
