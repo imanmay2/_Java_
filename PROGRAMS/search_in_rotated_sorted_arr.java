2730e4611b8f3455a296c139442f3be3b1bb8871
manmay2 <imanmay2@gmail.com>
2024-07-01 17:57:41 +0530

    bug yet to be configured out...

--- PROGRAMS/search_in_rotated_sorted_arr.java
+++ PROGRAMS/search_in_rotated_sorted_arr.java
@@ -1,21 +1,27 @@
 // Write a program , that will  search the target in the rotated sorted array.
 
 
-public class search_in_roated_sorted_arr{
+public class search_in_rotated_sorted_arr{
     public static int search(int arr[],int si,int target,int ei){
         int mid=(si+ei)/2;
-        if(arr[si]<arr[mid]){
-            if(target>arr[si] && target<arr[mid]){
-                return search(arr,si,target,mid-1);
-            }
-            else{
-                
-            }
+        if(arr[mid]==target){
+            return mid;
         }
+        else if(target>arr[mid]){
+            return search(arr,mid+1,target,ei);
+        }
+        else if(target<arr[mid]){
+            return search(arr,si,target,mid-1);
+        }
+        else if(si>ei){
+            System.out.println(si+" "+ ei);
+            return -1;
+        }
+        return 0;
     }
-    public static void main(String args){
-        target=0;
+    public static void main(String args[]){
+        int target=4;
         int arr[]={4,5,6,7,0,1,2};
-        System.out.println(search(arr,0,target,ei));
+        System.out.println(search(arr,0,target,arr.length-1));
     }
 }
