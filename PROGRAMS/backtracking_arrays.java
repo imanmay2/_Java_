66853e28165781bb05af6d4cb32c2b663ebc1fbb
manmay2 <imanmay2@gmail.com>
2024-07-06 20:39:53 +0530

    backtracking arrays

--- PROGRAMS/backtracking_arrays.java
+++ PROGRAMS/backtracking_arrays.java
@@ -1,33 +1,26 @@
-// Apply Backtracking on arrays...
+// Write a program that will bactrack the array...
 
 
-import java.util.*;
 public class backtracking_arrays{
-    public static void backtrack(int arr[],int i,int val){
+    public static void backtrack(int arr[],int i){
         //base case
         if(arr.length==i){
             print_Arr(arr);
+            System.out.println();
             return;
         }
-        arr[i]=val;
-        backtrack(arr,i+1,val+1);
+        arr[i]=i+1;
+        backtrack(arr,i+1);
         arr[i]=arr[i]-2;
     }
-
-
-
     public static void print_Arr(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
     }
-
-
-
-
     public static void main(String args[]){
         int arr[]=new int[5];
-        backtrack(arr,0,1);
+        backtrack(arr,0);
         print_Arr(arr);
     }
 }
