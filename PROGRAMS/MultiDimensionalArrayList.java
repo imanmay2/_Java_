f25230822978db4ffab94b508f73a248b3ddce98
manmay2 <imanmay2@gmail.com>
2024-07-10 00:58:57 +0530

    bug yet to be fixed

---
+++ PROGRAMS/MultiDimensionalArrayList.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class MultiDimensionalArrayList{
+    public static void main(String args[]){
+        ArrayList<ArrayList<Integer>> main_list=new ArrayList<>();
+        ArrayList<Integer> list1=new ArrayList<>();
+        ArrayList<Integer> list2=new ArrayList<>();
+        ArrayList<Integer> list3=new ArrayList<>();
+
+        for(int i=1;i<=5;i++){
+            list1.add(1*i);
+            list2.add(2*i);
+            list3.add(3*i);
+        }
+        main_list.add(list1);
+        main_list.add(list2);
+        main_list.add(list3);
+        for(int i=0;i<main_list.size();i++){
+            ArrayList<Integer> curr=new ArrayList<>();
+            for(int j=0;j<curr.size();j++){
+                System.out.println(curr.get(j));
+            }
+        }
+    }
+}
