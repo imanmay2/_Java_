f586bf3f504cffca9e6ae3bc03004fb3453d3413
manmay2 <imanmay2@gmail.com>
2024-07-11 12:36:55 +0530

    brute force of max conatining water..

--- PROGRAMS/max_Water_Container.java
+++ PROGRAMS/max_Water_Container.java
@@ -2,12 +2,12 @@
 
 import java.util.*;
 public class max_Water_Container{
-    public static void calc_Max(ArrayList<Integer> list,int width){
+    public static int calc_Max(ArrayList<Integer> list,int width){
         int max=Integer.MIN_VALUE;
         int m=0;
         for(int i=0;i<list.size();i++){
             for(int j=i+1;j<list.size();j++){
-                m=((j-i)*Math.min(list.get(i),list.get(j))*width);
+                m=((j-i)*(Math.min(list.get(i),list.get(j)))*width);
                 if(m>max){
                     max=m;
                 }
@@ -16,12 +16,15 @@ public class max_Water_Container{
         return max;
     }
     public static void main(String args[]){
-        Scanner sc=new Scanner();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
         ArrayList<Integer> list=new ArrayList<>();
-        for(int i=1;i<10;i++){
-            System.out.println("Enter the number to be inserted : ");
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the number to be added : ");
             list.add(sc.nextInt());
         }
-        System.out.println(calc_Max(list,1));
+
+        System.out.print("Maximum water conatining is : "+ calc_Max(list,1));
+
     }
 }
