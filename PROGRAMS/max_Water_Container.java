9c583f4abb80ed1ac01544b88325e7f3745d046e
manmay2 <imanmay2@gmail.com>
2024-07-11 13:18:11 +0530

    optimized code updated

--- PROGRAMS/max_Water_Container.java
+++ PROGRAMS/max_Water_Container.java
@@ -5,6 +5,9 @@ public class max_Water_Container{
     public static int calc_Max(ArrayList<Integer> list,int width){
         int max=Integer.MIN_VALUE;
         int m=0;
+
+
+        //Time Complexity : O(n^2).
         for(int i=0;i<list.size();i++){
             for(int j=i+1;j<list.size();j++){
                 m=((j-i)*(Math.min(list.get(i),list.get(j)))*width);
@@ -15,6 +18,25 @@ public class max_Water_Container{
         }
         return max;
     }
+
+    public static int optimized_Max(ArrayList<Integer> list,int width){
+        int m=0;
+
+
+        //Time Complexity: O(n).
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp<rp){
+            m=Math.max(m,(rp-lp)*(Math.min(list.get(rp),list.get(lp)))*width);
+            if(list.get(lp)<list.get(rp)){
+                lp++;
+            }
+            else{
+                rp--;
+            }
+        }
+        return m;
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -24,7 +46,8 @@ public class max_Water_Container{
             list.add(sc.nextInt());
         }
 
-        System.out.print("Maximum water conatining is : "+ calc_Max(list,1));
+        System.out.println("Maximum water conatining is : "+ calc_Max(list,1));
+        System.out.print("Maximum water conatining is : "+ optimized_Max(list,1));
 
     }
-}
+    }
