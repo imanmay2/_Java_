a81f83774842ce168635bc4e1fa54ae46fa3487c
manmay2 <imanmay2@gmail.com>
2024-07-11 14:36:35 +0530

    pair sum rotated sorted array done

--- PROGRAMS/pair_sum_ArrayList.java
+++ PROGRAMS/pair_sum_ArrayList.java
@@ -22,14 +22,39 @@ public class pair_sum_ArrayList{
             
         }
     }
+    public static void pair_sum_rotated_sorted(ArrayList<Integer> list,int target){
+        int bp=-1;
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i)>list.get(i+1)){
+                bp=i;
+                break;
+            }
+        }
+        int n=list.size();
+        int rp=bp;
+        int lp=bp+1;
+        while(lp!=rp){
+            if(target==list.get(lp)+list.get(rp)){
+                System.out.print("Match Found!! " + list.get(lp)+" "+list.get(rp));
+                break;
+            }
+            else if(list.get(lp)+list.get(rp)>target){
+                rp=(rp+n-1)%n;
+            }
+            else{
+                lp=(lp+1)%n;
+            }
+        }
+    }
     public static void main(String args[]){
         ArrayList<Integer> list=new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        sum_pair_1(list,5);
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        pair_sum_rotated_sorted(list,16);
 
     }
 }
