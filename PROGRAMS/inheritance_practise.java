d42a84b9b08a805f388d3c0c3a142ed619cbdf5c
manmay2 <imanmay2@gmail.com>
2024-07-27 20:05:40 +0530

    Inheritance Revised successfully.

--- PROGRAMS/inheritance_practise.java
+++ PROGRAMS/inheritance_practise.java
@@ -10,6 +10,18 @@ public class inheritance_practise{
         f1.swim();
         f1.breathe();
         f1.eat();
+
+        System.out.println("Inheritance Restarted !!!!");
+        System.out.println();
+
+        Dogs d1=new Dogs();
+        d1.breathe();
+        d1.reproduce();
+        d1.legs();
+
+
+
+
     }
 }
 
@@ -29,5 +41,19 @@ class Animal{               //Parent Class.
 class Fish extends Animal{      // Child Class---->Implementing Inheritance.
     void swim(){
         System.out.println("Fish swims");
+    }
+}
+
+
+class Mammals extends Animal{
+    void reproduce(){
+        System.out.println("Reproduce Sexually");
+    }
+}
+
+
+class Dogs extends Mammals{
+    void legs(){
+        System.out.println("Dog are  4 legged Mammals");
     }
 }
