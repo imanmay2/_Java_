415836e580d684f64ae4c1eaef9b7b65d53313e5
manmay2 <imanmay2@gmail.com>
2024-07-27 18:44:59 +0530

    updated

--- PROGRAMS/classes_objects.java
+++ PROGRAMS/classes_objects.java
@@ -1,9 +1,14 @@
 import java.util.*;
 public class classes_objects{
     public static void main(String args[]){
-        Pen p1=new Pen();
-        p1.setColor("Blue");
-        System.out.println(p1.color);
+        // Pen p1=new Pen();
+        // p1.setColor("Blue");
+        // System.out.println(p1.color);
+
+        bankAcc b1=new bankAcc();    // constructor.
+        b1.setPass("hello@1234");   // settters.
+        System.out.println(b1.getPass());   // getters.
+
     }
 }
 
@@ -13,5 +18,19 @@ class Pen{
     int tip;
     void setColor(String newColor){
         color=newColor;
+    }
+}
+
+
+
+class bankAcc{
+    public String username;
+    private String password;
+    public void setPass(String password){
+        this.password=password;
+    }
+
+    public String getPass(){
+        return this.password;
     }
 }
