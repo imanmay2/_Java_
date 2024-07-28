1dbfe1b9b34501eaede2df24c7cf95e14c7a9a24
manmay2 <imanmay2@gmail.com>
2024-07-28 17:41:46 +0530

    Node created

---
+++ PROGRAMS/LinkedList_basics.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LinkedList_basics{
+    public static void main(String args[]){
+
+    }
+}
+
+
+//Creating Nodes....:--->>> 
+
+
+public static class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public static Node head;
+public static Node tail;
+public static int size;
+
