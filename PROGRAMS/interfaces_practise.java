d737f20174b6d6d8aaeebc6a134e86517739c96c
manmay2 <imanmay2@gmail.com>
2024-07-28 17:05:08 +0530

    error encountered

--- PROGRAMS/interfaces_practise.java
+++ PROGRAMS/interfaces_practise.java
@@ -1,44 +1,19 @@
 import java.util.*;
 public class interfaces_practise{
     public static void main(String args[]){
-        Animal a1=new Animal();
-        System.out.println(a1.breathes());
+        Queen q1=new Queen();
+        System.out.println(q1.moves());
     }
 }
 
 
-
-interface func{
-    void breathes();
-    void eats();
-    void sleeps();
-    
+interface chessPlayer{
+    void moves();
 }
 
 
-class Animal implements func{
-    public void breathes(){
-        System.out.println("Animal breathes");
-    }
-
-    public void eats(){
-        System.out.println("Animal eats");
-    }
-
-    public void sleeps(){
-        System.out.println("Animal Sleeps");
+class Queen implements chessPlayer{
+    public void moves(){
+        System.out.println("King!!!!");
     }
 }
-
-
-// class Mammals extends Animal{
-//     void reproduce(){
-//         System.out.println("Reproduce Sexually");
-//     }
-// }
-
-// class Dogs extends Mammals{
-//     void legs(){
-//         System.out.println("Dogs have 4 legs");
-//     }
-// }
