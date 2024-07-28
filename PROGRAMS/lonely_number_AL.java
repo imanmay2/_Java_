ed0efb208082e2ffe9c6f4cf2ae4eb69166dcf82
manmay2 <imanmay2@gmail.com>
2024-07-28 19:12:45 +0530

    code yet to be deployed

--- PROGRAMS/lonely_number_AL.java
+++ PROGRAMS/lonely_number_AL.java
@@ -1,36 +1,13 @@
-// Find lonely number from the arrayList...given..
+// Display the lonely numbers in the arraylist.
+
 
 import java.util.*;
 public class lonely_number_AL{
+    public static ArrayList<Integer> 
+
+
+
     public static void main(String args[]){
-        ArrayList<Integer> list=new ArrayList<>();
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the range of the ArrayList : ");
-        int n=sc.nextInt();
-
-
-        //Adding elements in the ArrayList...
-        for(int i=0;i<n;i++){
-            System.out.println("Enter the number to be inserted : ");
-            list.add(sc.nextInt());
-        }
-
-
-        for(int i=0;i<list.size();i++){
-            for(int j=0;j<list.size();j++){
-                if(i==j){
-                    continue;
-                }
-                else{
-                    if((list.get(i)+1)==list.get(j) || (list.get(i)-1)==list.get(j) || list.get(i)=y=list.get(j)){
-                        System.out.print(list.get(i)+" ");
-                    }
-                    // else{
-                    //     System.out.print(list.get(i)+" ");
-                    // }
-                }
-            }
-        }
-
+        
     }
 }
