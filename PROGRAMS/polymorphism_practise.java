a1df5080f006a934cbc954ed3024945d9a773d4d
manmay2 <imanmay2@gmail.com>
2024-07-28 14:24:24 +0530

    updated

--- PROGRAMS/polymorphism_practise.java
+++ PROGRAMS/polymorphism_practise.java
@@ -2,21 +2,25 @@ import java.util.*;
 public class polymorphism_practise{
     public static void main(String args[]){
         Dear d1=new Dear();
-        d1.eat();       //function overwriting will take place.
+        d1.eat();       //function overriding will take place.
+
+        Calculator c=new Calculator();
+        System.out.println(c.sum(1,2));
+        System.out.print(c.sum(1,2,3));
     }
 }
 
 
 
-// class Calculator {
-//     int sum(int a,int b){
-//         return a+b;
-//     }
+class Calculator {
+    int sum(int a,int b){
+        return a+b;
+    }
 
-//     float sum(float a,float b){
-//         return a+b;
-//     }
-// }
+    int sum(int a,int b,int c){
+        return (a+b+c);
+    }
+}
 
 class Animal{
     void eat(){
