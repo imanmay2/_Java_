4b1f5e4501b9d72751f9171034b501c03bc80ceb
manmay2 <imanmay2@gmail.com>
2024-07-28 18:08:42 +0530

    updated Add First

--- PROGRAMS/LinkedList_basics.java
+++ PROGRAMS/LinkedList_basics.java
@@ -1,7 +1,10 @@
 import java.util.*;
 public class LinkedList_basics{
     public static void main(String args[]){
-
+        addFirst(4);
+        addFirst(3);
+        addFirst(2);
+        addFirst(1);
     }
 }
 
@@ -20,4 +23,17 @@ public static class Node{
 public static Node head;
 public static Node tail;
 public static int size;
+
+
+// Add_First in the linked list.(Head wil change every time).
+public void addFirst(int data){
+    Node newNode=new Node(data);
+    if(head==null){
+        head=tail=newNode;
+        return;
+    }
+    newNode.next=head;
+    head=newNode;
+}
+
 
