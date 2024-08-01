5584aecae1766c6b4f5fa6eab52a523490f8987e
manmay2 <imanmay2@gmail.com>
2024-08-01 07:15:07 +0530

    code deployed successfully

--- PROGRAMS/LinkedList.java
+++ PROGRAMS/LinkedList.java
@@ -10,6 +10,7 @@ public class LinkedList{
 
     public static Node head;        //creating head node.
     public static Node tail;        //creating tail Node.
+    public static int size;
 
     public void addFirst(int data){
         Node newNode=new Node(data);
@@ -19,6 +20,7 @@ public class LinkedList{
         }
         newNode.next=head;
         newNode=head;
+        size++;
     }
 
 
@@ -29,9 +31,9 @@ public class LinkedList{
             head=tail=newNode;
             return;
         }
-
         tail.next=newNode;
         tail=newNode;
+        size++;
     }
 
 
@@ -44,14 +46,40 @@ public class LinkedList{
         System.out.print("Null");
     }
 
+    
+
+    public void addElement(int idx,int data){
+        if(idx==0){
+            addFirst(data);
+            size++;
+            return;
+        }
+        Node newNode=new Node(data);
+        Node temp=head;
+        int i=0;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+        size++;
+    }
+
 
     public static void main(String args[]){
         LinkedList ll=new LinkedList();
         ll.addFirst(1);
-        ll.addFirst(2);
+        ll.addFirst(2);     //this is not calling...
         ll.addLast(3);
         ll.addLast(4);
         ll.addLast(5);
         ll.print_LL();
+        System.out.println();
+        ll.addElement(1,6);
+        ll.print_LL();
+        System.out.println("The size of the LinkedList is : "+size);
+
+
     }
 }
