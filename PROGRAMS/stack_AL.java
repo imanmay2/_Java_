603353d1fef9af8ba6c60a48316deb676044100c
manmay2 <imanmay2@gmail.com>
2024-08-08 19:22:38 +0530

    bug yet to be fixed

---
+++ PROGRAMS/stack_AL.java
@@ -0,0 +1,46 @@
+// Implementing Stack using ArrayList.
+
+import java.util.*;
+public class stack_AL{
+    static class stack{
+        static ArrayList<Integer> list=new ArrayList<>();
+
+
+        public static void push(int n){
+            list.add(n);
+            System.out.println("Item pushed!!");
+        }
+
+
+        public static void peek(){
+            if(list.size()==0){
+                System.out.println("List is empty.");
+                return;
+            }
+            System.out.println("The Peek element of the Stack is "+list.get(list.size()-1));
+        }
+
+
+        public static int pop(){
+            if(list.size()==0){
+                System.out.println("Stack is empty");
+                return 0;
+            }
+            int p=list.size()-1;
+            System.out.println("The poped element is "+list.remove(list.size()-1));
+            return p;
+        }
+
+        
+    }
+    public static void main(String args[]){
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while(s.size()!=0){
+            s.pop();
+        }
+    }
+}
