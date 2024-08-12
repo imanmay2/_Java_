831f907c86703f40235d04ccfabc640c6a4c48cf
manmay2 <imanmay2@gmail.com>
2024-08-13 00:48:54 +0530

    yet to be done...

---
+++ PROGRAMS/stock_span_problem.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class stock_span_problem {
+    public static void main(String args[]){
+        Stack<Integer> s=new Stack<>();
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(100);
+        list.add(80);
+        list.add(60);
+        list.add(70);
+        list.add(60);
+        list.add(85);
+        list.add(100);
+        ArrayList<Integer> span=new ArrayList<>();
+        System.out.println("The Stock is as follows : ");
+    }
+}
