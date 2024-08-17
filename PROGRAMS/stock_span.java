d78a07a8c632a2f5e2afd53d00c0888dd11d7b21
manmay2 <imanmay2@gmail.com>
2024-08-17 11:36:57 +0530

    code updated

---
+++ PROGRAMS/stock_span.java
@@ -0,0 +1,13 @@
+// Write a program that will count the Stocks and Span...
+
+import java.util.*;
+public class stock_span{
+    public static void calc_stock(int[] arr){
+        int[] span=new int[arr.length];
+        
+    }
+    public static void main(String args[]){
+        int[] arr={100,80,60,70,60,85,100};
+        calc_stock(arr);
+    }
+}
