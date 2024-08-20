f31aec9f2f597e52542b71b9a039946e5b90af85
manmay2 <imanmay2@gmail.com>
2024-08-20 20:33:06 +0530

    code deployed

--- PROGRAMS/stock_span.java
+++ PROGRAMS/stock_span.java
@@ -1,22 +1,36 @@
-// Write a Program that will display the stock.
+// Write a program that will calculate the Stock of the company. And display the span array.
 
 import java.util.*;
 public class stock_span{
-    public static void calc_stock(int[] arr,int[] span){
+
+    public static void calc_Stock(int[] arr,int[] span){
         Stack<Integer> st=new Stack<>();
-        span[0]=1;
-        st.push(0);
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]<arr[st.peek()]){
-                st.push(i);
-                span[i]=i-st.peek();
+        int i=0;
+        span[i]=1;
+        st.push(i);
+        while(!st.isEmpty()){
+            i++;
+            if(i==arr.length-1){
+                span[i]=i+1;
             }
-            
+            else if(arr[st.peek()]>arr[i]){
+                span[i]=i-st.peek();
+                st.push(i);
+            }
+            else if(arr[st.peek()]<arr[i]){
+                st.pop();
+            }
         }
+
     }
     public static void main(String args[]){
         int[] arr={100,80,60,70,60,85,100};
         int[] span=new int[arr.length];
-        calc_stock(arr,span);
+        calc_Stock(arr,span);
+
+        //printing the span array.
+        for(int i=0;i<span.length;i++){
+            System.out.println(span[i]+" ");
+        }
     }
 }
