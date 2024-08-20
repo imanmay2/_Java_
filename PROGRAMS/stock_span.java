a3f71a60f08b02cd789ca1b19318bf169a13507e
manmay2 <imanmay2@gmail.com>
2024-08-20 21:29:56 +0530

    finally code uploaded

--- PROGRAMS/stock_span.java
+++ PROGRAMS/stock_span.java
@@ -2,33 +2,29 @@
 
 import java.util.*;
 public class stock_span{
-
     public static void calc_Stock(int[] arr,int[] span){
         Stack<Integer> st=new Stack<>();
-        int i=0;
-        span[i]=1;
-        st.push(i);
-        while(!st.isEmpty()){
-            i++;
-            if(i==arr.length-1){
+        st.push(0);
+        span[0]=1;
+        for(int i=1;i<span.length;i++){
+            while(!st.isEmpty() && arr[i]>arr[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
                 span[i]=i+1;
             }
-            else if(arr[st.peek()]>arr[i]){
+            else{
                 span[i]=i-st.peek();
                 st.push(i);
             }
-            else if(arr[st.peek()]<arr[i]){
-                st.pop();
-            }
         }
-
     }
     public static void main(String args[]){
         int[] arr={100,80,60,70,60,85,100};
         int[] span=new int[arr.length];
         calc_Stock(arr,span);
 
-        //printing the span array.
+        // printing the span array.
         for(int i=0;i<span.length;i++){
             System.out.println(span[i]+" ");
         }
