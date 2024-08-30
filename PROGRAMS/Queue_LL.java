08eae4c32b57bb9c0613e30456305c98c2d7f33e
manmay2 <imanmay2@gmail.com>
2024-08-30 12:46:39 +0530

    code yet to be run

--- PROGRAMS/Queue_LL.java
+++ PROGRAMS/Queue_LL.java
@@ -1,3 +1,67 @@
+//Implementing Stack using 
+
+import java.util.*;
 public class Queue_LL {
+    static class Node{
+        static int data;
+        static Node next;
+        static int size=-1;
+        public Node(int data){     //constructor.
+            this.data=data;
+            this.next=null;
+        }
+
+        //just initialization.
+        public static Node head;
+        public static Node tail;
+
+        //Adding element in the Queue.
+        public static void add(int data){
+            Node newNode=new Node(data);
+            size++;
+            if(head==null){
+                head=tail=newNode;
+                return;
+            }
+            tail.next=newNode;
+            tail=newNode;
+        }
+        //Removing element from the Queue.
+        public static int remove(){
+            int val;
+            if(size==-1){
+                System.out.println("Queue is Empty !!");
+                return -1;
+            }
+
+
+            //if there is only 1 element in the Queue.
+            if(size==0){
+                val=head.data;
+                head=tail=null;
+                size--;
+                return val;
+            }
+            val=head.data;
+            head=head.next;
+            size--;
+            return val;
+        }
+
+
+        public static int peek(){
+            if(size==-1){
+                System.out.println("Queue is Empty. ");
+                return -1;
+            }
+
+            return head.data;
+        }
+
+    }
+
     
+    public static void main(String args[]){
+
+    }
 }
