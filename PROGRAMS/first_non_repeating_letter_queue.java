f1e5fe6bdb9e7ee4514f9de63782c0b3e3177169
manmay2 <imanmay2@gmail.com>
2024-08-31 23:03:42 +0530

    code yet to be coded

--- PROGRAMS/first_non_repeating_letter_queue.java
+++ PROGRAMS/first_non_repeating_letter_queue.java
@@ -5,28 +5,6 @@
 import java.util.*;
 public class first_non_repeating_letter_queue{
 
-    public static char func_(String str){
-        Queue<Character> q1=new ArrayDeque<>();
-        char[] arr=new char[26];
-        char s_;
-        q1.add(str.charAt(0));
-        arr[str.charAt(0)-'a']++;
-        s_=str.charAt(0);
-        for(int i=1;i<str.length();i++){
-            q1.add(str.charAt(i));
-            arr[str.charAt(i)-'a']++;
-            while(arr[q1.peek()-'a']>1 && !q1.isEmpty()) {
-                q1.remove();
-            }
-            if(!q1.isEmpty()){
-                s_=(char) (s_+q1.peek());
-            }
-        }
-        return s_;
-        
-    }
-    public static void main(String args[]){
-        String str=new String("aabccxb");
-        System.out.println(func_(str));
-    }
+   
+    
 }
