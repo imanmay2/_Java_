2f3863eb9e43b41ed96d2fd1b870847e321a88ba
manmay2 <imanmay2@gmail.com>
2024-08-31 12:22:27 +0530

    yet to be coded

---
+++ PROGRAMS/Stack_using_2Queue.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Stack_using_2Queue {
+    public static class Stack{
+        static Queue<Integer> q1=new ArrayDeque<>();
+        static Queue<Integer> q2=new ArrayDeque<>();
+        static int ct_add=0;
+        
+        //Add()---
+        
+
+    }
+    
+    public static void main(String args[]){
+
+    }
+}
