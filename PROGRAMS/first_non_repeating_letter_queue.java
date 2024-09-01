e93b8aea642595fb6ae77c32d75934bd12202826
manmay2 <imanmay2@gmail.com>
2024-09-01 10:31:46 +0530

    code solved finally...

--- PROGRAMS/first_non_repeating_letter_queue.java
+++ PROGRAMS/first_non_repeating_letter_queue.java
@@ -3,8 +3,30 @@
 // Output : a -1 b b b b x. 
 
 import java.util.*;
+import java.util.LinkedList;
 public class first_non_repeating_letter_queue{
-
-   
-    
+    public static void func_(String str){
+        Queue<Character> q1=new LinkedList<>();
+        int[] arr=new int[26];
+        q1.add(str.charAt(0));
+        arr[str.charAt(0)-'a']++;
+        System.out.print(q1.peek()+" ");
+        for(int i=1;i<str.length();i++){
+            char ch=str.charAt(i);
+            q1.add(ch);
+            arr[ch-'a']++;
+            while(!q1.isEmpty() && arr[q1.peek()-'a']>1){
+                q1.remove();
+            }
+            if(q1.isEmpty()){
+                System.out.print("-1 ");
+                continue;
+            }
+            System.out.print(q1.peek()+" ");
+        }
+    }
+    public static void main(String args[]){
+        String str="aabccxb";
+        func_(str);
+    }
 }
