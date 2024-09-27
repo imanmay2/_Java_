848111857fc2e94db6c691b633a31194cd22f6fa
manmay2 <imanmay2@gmail.com>
2024-09-27 20:20:25 +0530

    Binary Tree part one finished....

--- PROGRAMS/Binary_Trees.java
+++ PROGRAMS/Binary_Trees.java
@@ -15,6 +15,9 @@ public class Binary_Trees {
 
     static class binaryTree{
         static int idx=-1;
+
+
+        //Creating the binary tree.
         public static Node buildTree(int[] arr){
             idx++;
             if(arr[idx]==-1){
@@ -27,7 +30,7 @@ public class Binary_Trees {
         }
 
 
-
+        //Pre-order Traversal
         public static void pre_order_Traversal(Node root){
             if(root==null){
                 return;
@@ -38,7 +41,7 @@ public class Binary_Trees {
         }
 
 
-
+        //In order Traversal
         public static void in_order_traversal(Node root){
             if(root==null){
                 return;
@@ -50,6 +53,7 @@ public class Binary_Trees {
 
 
 
+        //Post order Traversal
         public static void post_order_traversal(Node root){
             if(root==null){
                 return;
@@ -59,7 +63,9 @@ public class Binary_Trees {
             System.out.print(root.data+" ");
         }
 
+        
 
+        // Level Order Traversal
         public static void level_order_traversal(Node root){
             if(root==null){
                 return;
@@ -87,11 +93,12 @@ public class Binary_Trees {
                         q1.add(null);
                     }
                 }
-            }
-
-            
+            } 
         }
 
+
+
+        // Count of the number of the nodes present in the Binary Tree.
         public static int count_Nodes(Node root){
             if(root==null){
                 return 0;
@@ -100,7 +107,30 @@ public class Binary_Trees {
             int right_count=count_Nodes(root.right);
             return left_count+right_count+1;
         }
+
+        
+        //Sum of the Nodes in the Binary Tree.
+        public static int sum_Nodes(Node root){
+            if(root==null){
+                return 0;
+            }
+            int left_sum=sum_Nodes(root.left);
+            int right_sum=sum_Nodes(root.right);
+            return left_sum+right_sum+root.data;
+        }
+
+    //Height of the Binary Tree.
+    public static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        int left_subtree=height(root.left);
+        int right_subtree=height(root.right);
+        return (Math.max(left_subtree,right_subtree)+1);
     }
+    }
+
+    
 
     
 
@@ -108,7 +138,7 @@ public class Binary_Trees {
 
     public static void main(String args[]){
         int[] arr={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        Node root=binaryTree.buildTree(arr);
+        Node root=binaryTree.buildTree(arr); //Where is the object created ???? First
         // System.out.println(root.data);
         // System.out.println();
         // binaryTree.pre_order_Traversal(root);
@@ -118,5 +148,12 @@ public class Binary_Trees {
         // binaryTree.post_order_traversal(root);
         // binaryTree.level_order_traversal(root);
         // System.out.println(binaryTree.count_Nodes(root));
+        // System.out.println(binaryTree.sum_Nodes(root));
+        System.out.println(binaryTree.height(root));
     }
 }
+
+
+
+
+
