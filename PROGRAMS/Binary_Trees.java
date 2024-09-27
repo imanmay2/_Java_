1ecea8427868546c38501733ed4e54b5cb3861d5
manmay2 <imanmay2@gmail.com>
2024-09-27 20:21:00 +0530

    Binary Trees part-1 terminated....

--- PROGRAMS/Binary_Trees.java
+++ PROGRAMS/Binary_Trees.java
@@ -130,12 +130,6 @@ public class Binary_Trees {
     }
     }
 
-    
-
-    
-
-
-
     public static void main(String args[]){
         int[] arr={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node root=binaryTree.buildTree(arr); //Where is the object created ???? First
