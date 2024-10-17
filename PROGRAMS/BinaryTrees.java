7ea9b8ad12037f23475548289d85fa935122e48e
manmay2 <imanmay2@gmail.com>
2024-10-17 22:07:57 +0530

    Binary Tree reboot

--- PROGRAMS/BinaryTrees.java
+++ PROGRAMS/BinaryTrees.java
@@ -1,3 +1,37 @@
-                                                            // Practising Binary Trees, basics///
+                                                 // Practising Binary Trees, basics///
 
 
+public class BinaryTrees{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+
+    static class Binary_Tree{
+        static int idx=-1;
+        public static Node buildTree(int nodes[]){
+            // Building up the Binary Tree.
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+    }
+    public static void main(String args[]){
+        int nodes[]={10,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Binary_Tree tree=new Binary_Tree();
+        Node root=tree.buildTree(nodes);
+        System.out.println("The root is : "+ root.data);
+    }
+}
