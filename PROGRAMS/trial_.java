00a28d9558d4f8f38e1fe4c0db79e623bdfcdb69
manmay2 <imanmay2@gmail.com>
2024-10-20 09:02:31 +0530

    Programming Initiated

--- PROGRAMS/trial_.java
+++ PROGRAMS/trial_.java
@@ -1,7 +1,8 @@
 import java.util.Date;
 public class trial_{
+    public static 
     public static void main(String[] args) {
         Date d1 = new Date();
-        System.out.println("Current date is " + d1.toString());
+        System.out.println("Current date is " + d1.toString()); 
     }
 }
