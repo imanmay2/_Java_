2103b2b1c061e594522e4edd33bbe9369d090e10
manmay2 <imanmay2@gmail.com>
2024-11-09 21:06:34 +0530

    Will Do it later

---
+++ PROGRAMS/binaryTree_practise.java
@@ -0,0 +1,89 @@
+import java.util.Queue;
+
+public class binaryTree_practise{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+
+    static class binaryTree{
+        static int idx=-1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+
+        public static void preOrderTraversal(Node root){
+            if(root==null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preOrderTraversal(root.left);
+            preOrderTraversal(root.right);
+        }
+
+
+        public static void inOrderTraversal(Node root){
+            if(root==null){
+                return;
+            }
+            inOrderTraversal(root.left);
+            System.out.print(root.data+" ");
+            inOrderTraversal(root.right);
+        }
+
+        public static void postOrderTraversal(Node root){
+            if(root==null){
+                return;
+            }
+            postOrderTraversal(root.left);
+            postOrderTraversal(root.right);
+            System.out.print(root.data+" ");
+        }
+
+
+        public static void levelOrderTraversal(Node root){
+            if(root==null){
+                return;
+            }
+            Queue<Node> q=new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode=q.remove();   //Deletes the node.
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        binaryTree tree=new binaryTree();
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node root=tree.buildTree(nodes);
+        System.out.println("Root is : "+root.data);
+        tree.preOrderTraversal(root);
+        System.out.println();
+        tree.inOrderTraversal(root);
+        System.out.println();
+        tree.postOrderTraversal(root);
+    }
+}
