74ee409fe1051289ae5f4295d6358ad31b98f814
manmay2 <imanmay2@gmail.com>
2024-11-11 21:27:43 +0530

    Updated 2.0

--- PROGRAMS/binaryTree_practise.java
+++ PROGRAMS/binaryTree_practise.java
@@ -100,6 +100,7 @@ public class binaryTree_practise{
             return countNode(root.left)+countNode(root.right)+1;
         }
     }
+    
     public static void main(String args[]){
         binaryTree tree=new binaryTree();
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
