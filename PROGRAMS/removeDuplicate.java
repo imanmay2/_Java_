b64557ce832a78937b874c9d93bff2257fa4eaa0
manmay2 <imanmay2@gmail.com>
2024-11-17 16:43:01 +0530

    updated

---
+++ PROGRAMS/removeDuplicate.java
@@ -0,0 +1,27 @@
+public class removeDuplicate {
+    public static boolean check(String s,String str){
+        for(int i=0;i<str.length();i++){
+            if(s.equals(str.charAt(i)+"")){
+                System.out.println("enter");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    
+    public static int removeDuplicates(int[] nums) {
+        String str=",";
+        for(int i=0;i<nums.length;i++){
+            String s=Integer.toString(nums[i]);
+            if(!check(s,str)){
+                str+=s;
+            }
+        }
+
+        return str.length()-1;
+    }
+    public static void main(String args[]){
+        System.out.println(removeDuplicates(new int[]{1,1,1,2}));
+    }
+}
