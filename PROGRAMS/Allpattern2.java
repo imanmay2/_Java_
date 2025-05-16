ebe89c68071be4545af548be47a47158600176f4
manmay2 <imanmay2@gmail.com>
2025-05-17 03:44:56 +0530

    Java comeback ...

--- PROGRAMS/Allpattern2.java
+++ PROGRAMS/Allpattern2.java
@@ -14,9 +14,43 @@ public class Allpattern2 {
 
 
     public static void hollowRightTrianglePattern(int n){
-        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if((i+j)==(n+1) || i==n || j==n){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            } System.out.println();
+        }
     }
+
+
+    public static void zeroOnePattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }else if((i+j)%2!=0){
+                    System.out.print("0");
+                }
+            }System.out.println();
+        }
+    }
+
+    public static void Xpattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==j || (i+j)==n+1){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            } System.out.println();
+        }
+    }
+
     public static void main(String args[]){
-        rightTrianglePattern(6);
+        Xpattern(5);
     }
 }
