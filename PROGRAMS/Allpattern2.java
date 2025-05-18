f62a4c40596eb0900e683ab1f1cca47aaf8e62aa
manmay2 <imanmay2@gmail.com>
2025-05-18 19:07:59 +0530

    butterfly pattern updated

--- PROGRAMS/Allpattern2.java
+++ PROGRAMS/Allpattern2.java
@@ -50,7 +50,56 @@ public class Allpattern2 {
         }
     }
 
+    public static void flyodTriangle(int n){
+        int k=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterflyPattern(int n){
+
+        // for the upper part.
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+
+            for(int m=1;m<=i;m++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+
+        //for the lower part.
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+
+            for(int m=1;m<=i;m++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]){
-        Xpattern(5);
+        butterflyPattern(4);
     }
 }
