1aa4667e3546abc09587fffa59e7ec160813c639
manmay2 <imanmay2@gmail.com>
2025-05-18 22:11:31 +0530

    linear search

---
+++ PROGRAMS/array_Part1.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class array_Part1{
+
+    public static void inputArr(int n,int[] arr){
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<n;i++){
+            System.out.print("Enter the element: ");
+            arr[i]=sc.nextInt();
+        }
+        System.out.println();
+    }
+
+
+    public static void printArr(int n,int[] arr){
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println();
+    }
+
+    public static void linearSearch(int n,int[] arr){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the key value : ");
+        int k=sc.nextInt();
+        boolean f=false;
+        for(int i=0;i<n;i++){
+            if(arr[i]==k){
+                System.out.print("Key Found! at position : "+i+1);
+                f=true;
+                break;
+            }
+        }
+        if(f==false){
+            System.out.println("Key not found in the Array!");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the range of the array: ");
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        inputArr(n, arr);
+
+        linearSearch(n, arr);
+        printArr(n, arr);
+
+        
+    }
+}
