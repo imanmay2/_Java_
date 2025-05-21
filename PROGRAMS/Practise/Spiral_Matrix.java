f11321f917647d5f5c7c2e3f02f12005ba0cb0db
manmay2 <imanmay2@gmail.com>
2025-05-21 21:02:14 +0530

    spiral Matrix done

--- PROGRAMS/Practise/Spiral_Matrix.java
+++ PROGRAMS/Practise/Spiral_Matrix.java
@@ -2,34 +2,38 @@ public class Spiral_Matrix {
 
 
     public static void Spiral_Matrix(int arr[][]){
-        startRow=0;
-        endRow=arr.length-1;
+        int startRow=0;
+        int endRow=arr.length-1;
 
-        startCol=0;
-        endCol=arr[0].length-1;
-
+        int startCol=0;
+        int endCol=arr[0].length-1;
+ 
         while(startRow<=endRow && startCol<=endCol){
 
             //top border;
             for(int j=startCol;j<=endCol;j++){
-                System.out.print(arr[j]+" ");
+                System.out.print(arr[startRow][j]+" ");
             }
 
             //right border.
-            for(int i=startRow+1;i<endRow;i++){
-                System.out.print(arr[i]+" ");
+            for(int i=startRow+1;i<=endRow;i++){
+                System.out.print(arr[i][endCol]+" ");
             }
 
             //bottom border.
-            for(int j=endCol-1;>=startCol;j--){
-                System.out.print(arr[j]+" ");
+            for(int j=endCol-1;j>=startCol;j--){
+                System.out.print(arr[endRow][j]+" ");
             }
 
             //left border.
             for(int i=endRow-1;i>startRow;i--){
-                System.out.print(arr[i]+" ");
+                System.out.print(arr[i][startCol]+" ");
             }
 
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
         }
     }
     public static void main(String args[]){
@@ -38,9 +42,8 @@ public class Spiral_Matrix {
             {5,6,7,8},
             {9,10,11,12},
             {13,14,15,16}
-        }
+        };
 
-
-        
+        Spiral_Matrix(arr);
     }
 }
