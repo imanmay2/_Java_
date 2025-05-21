52b3541853d1f7bcfc7257e73e4b99073da45857
manmay2 <imanmay2@gmail.com>
2025-05-21 22:22:22 +0530

    Search_ in a sorted array

--- PROGRAMS/Practise/Search_Sorted_Matrix.java
+++ PROGRAMS/Practise/Search_Sorted_Matrix.java
@@ -1,6 +1,26 @@
 public class Search_Sorted_Matrix {
 
-    
+    public static void Search_Sorted_Matrix(int arr[][],int key){
+        int row=0;
+        int col=arr[0].length-1;
+        boolean f=false;
+        while(row<arr.length && col>=0){
+            if(key>arr[row][col]){
+                row++;
+            } else if(key<arr[row][col]){
+                col--;
+            } else{
+                f=true;
+                System.out.println("Element found at index: "+row+","+col);
+                break;
+            }
+        }
+
+        if(f==false){
+            System.out.println("So sorry,Element not found in the matrix.");
+        }
+
+    }
     public static void main(String args[]){
         int arr[][]={
             {1,2,3,4,5},
@@ -9,5 +29,7 @@ public class Search_Sorted_Matrix {
             {16,17,18,19,20},
             {21,22,23,24,25}
         };
+
+        Search_Sorted_Matrix(arr, 23);
     }
 }
