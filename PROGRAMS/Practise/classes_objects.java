04eccf07d029f18a5094571af7fde8fdc40de166
manmay2 <imanmay2@gmail.com>
2025-05-24 23:26:05 +0530

    to be tried on next day morning

---
+++ PROGRAMS/Practise/classes_objects.java
@@ -0,0 +1,24 @@
+public class classes_objects {
+    public static void main(String args[]){
+        Greet g1=new Greet();
+        Greet.setName("Anwesha");
+        System.out.print("Name you have entered is "+g1.getName());
+    }
+}
+
+
+class Greet{
+    String color;
+    String name;
+    Greet(){
+        System.out.println("Hello , Java. welcome to Object oriented programming.");
+    }
+
+    void setName(String name){
+        this.name=name;
+    }
+
+    String getName(){
+        return this.name;
+    }
+}
