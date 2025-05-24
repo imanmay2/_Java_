1acbf9b9d5dd746f2a91ab5cae692e6e93cdaf23
manmay2 <imanmay2@gmail.com>
2025-05-24 08:50:37 +0530

    largest string in the sentence

--- PROGRAMS/Practise/charArrString.java
+++ PROGRAMS/Practise/charArrString.java
@@ -2,7 +2,14 @@ public class charArrString {
     public  static void main(String args[]){
         String str="Hello this is Java Tutorial";
         //printing the maximum largest string from the given sentence.
-        String arr[]=
+        String arr[]=str.split(" ");
+        int maxPosition=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].length()>maxPosition){
+                maxPosition=i;
+            }
+        }
 
-    }
+        System.out.println("Largest String in the Sentence is: "+arr[maxPosition]);
+        }
 }
