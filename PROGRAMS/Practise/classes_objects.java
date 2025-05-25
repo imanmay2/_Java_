86cb558f996651838eae3d74fe991fd83e4dbc5f
manmay2 <imanmay2@gmail.com>
2025-05-25 07:39:00 +0530

    Inroduction to classes and objects

--- PROGRAMS/Practise/classes_objects.java
+++ PROGRAMS/Practise/classes_objects.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class classes_objects {
     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the name of the Student: ");
+        String name=sc.nextLine();
         Greet g1=new Greet();
-        g1.setName("Anwesha");
+        g1.setName(name);
         System.out.print("Name you have entered is "+g1.getName());
     }
 }
