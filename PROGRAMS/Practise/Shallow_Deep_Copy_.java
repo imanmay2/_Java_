615f3188026ab43ab83fc8e4478369cef6e7c7e3
manmay2 <imanmay2@gmail.com>
2025-05-25 08:40:08 +0530

    Shallow_Deep_copy_yet to be understand

---
+++ PROGRAMS/Practise/Shallow_Deep_Copy_.java
@@ -0,0 +1,52 @@
+public class Shallow_Deep_Copy_ {
+    public static void main(String args[]){
+        Student s1=new Student("Anwesha",1,new int[]{98,99,100});
+        
+
+
+        Student s2=new Student(s1);
+        // System.out.println(s2.name);
+        // System.out.println(s2.roll);
+
+        // for(int i=0;i<3;i++){
+        //     System.out.print(s2.marks[i]+" ");
+        // }
+
+        s1.print(s2);
+        s2.name="Anwesha Chakraborty";
+        s1.print(s1);
+        System.out.println();
+        System.out.println(s1.name);
+
+
+    }
+}
+
+class Student{
+    String name;
+    int roll;
+    int marks[]=new int[3];
+
+    Student(String name,int roll,int marks[]){
+        this.name=name;
+        this.roll=roll;
+        for(int i=0;i<marks.length;i++){
+            this.marks[i]=marks[i];
+        }
+    }
+
+    Student(Student s1){
+        this.name=s1.name;
+        this.roll=s1.roll;
+        this.marks=s1.marks;
+    }
+
+
+    public void print(Student s){
+        System.out.println(s.name);
+        System.out.println(s.roll);
+        for(int i=0;i<s.marks.length;i++){
+            System.out.print(s.marks[i]+" ");
+        }
+    }
+}
