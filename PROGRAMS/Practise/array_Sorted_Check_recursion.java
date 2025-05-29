10faa5329e59951f80cd4086d65ec98461635dd6
manmay2 <imanmay2@gmail.com>
2025-05-29 12:06:21 +0530

    yet to be done

---
+++ PROGRAMS/Practise/array_Sorted_Check_recursion.java
@@ -0,0 +1,5 @@
+public class array_Sorted_Check_recursion {
+    public static void main(String args[]){
+        
+    }
+}
--- PROGRAMS/Practise/fibonaaciRecursion.java
+++ PROGRAMS/Practise/fibonaaciRecursion.java
@@ -1,12 +1,10 @@
 public class fibonaaciRecursion {
-
     public static int Fibonacci(int n){
         if(n==1){
             return 1;
         } else if(n==0){
             return 0;
         }
-
         return Fibonacci(n-1)+Fibonacci(n-2);
     }
     public static void main(String args[]){
