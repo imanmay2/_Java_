7e5b7225627c65909b9e23bf929ff28cdf23a941
manmay2 <imanmay2@gmail.com>
2025-05-29 16:11:56 +0530

    First Occurence of a key

--- PROGRAMS/Practise/array_Sorted_Check_recursion.java
+++ PROGRAMS/Practise/array_Sorted_Check_recursion.java
@@ -1,5 +1,17 @@
 public class array_Sorted_Check_recursion{
+    public static boolean isSorted(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
+        }
+
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+
+        return isSorted(arr, i+1);
+    }
     public static void main(String args[]){
-        
+        int arr[]={4,3};
+        System.out.println(isSorted(arr, 0));
     }
 }
