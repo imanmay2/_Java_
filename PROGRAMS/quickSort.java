e348fd7bf50e58a835b977d88c44466b668f62c3
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-01 21:03:36 +0530

    continue from mac

--- PROGRAMS/quickSort.java
+++ PROGRAMS/quickSort.java
@@ -2,7 +2,13 @@ public class quickSort {
 
     public static int pivotIdx(int arr[],int si,int ei){
         int i=si-1;
-        
+
+        for(int j=si;j<ei;j++){
+            if(arr[j]<arr[ei]){
+
+                
+            }
+        }
     }
     public static void quickSort_(int arr[],int si,int ei){
         
