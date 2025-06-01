c60ecb2630d5aead79d84c059e8c85897cd3444e
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-01 20:58:13 +0530

    yet to be done

---
+++ PROGRAMS/quickSort.java
@@ -0,0 +1,21 @@
+public class quickSort {
+
+    public static int pivotIdx(int arr[],int si,int ei){
+        int i=si-1;
+        
+    }
+    public static void quickSort_(int arr[],int si,int ei){
+        
+        ///base case
+        if(si>=ei){
+            return;
+        }
+
+
+
+    }
+    public static void main(String[] args) {
+        int arr[]={6,3,9,8,2,5};
+
+    }
+}
