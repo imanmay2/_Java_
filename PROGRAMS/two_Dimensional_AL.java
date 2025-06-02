2b24e4c8ca395c627c2eadf2f2f19219e2509379
manmay2 <imanmay2@gmail.com>
2025-06-02 22:09:08 +0530

    2D ArrayList , yet to be done

--- PROGRAMS/two_Dimensional_AL.java
+++ PROGRAMS/two_Dimensional_AL.java
@@ -1,17 +1,31 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class two_Dimensional_AL {
-    public static void main(String args[]){
-        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
-        int rows=3;
-        int cols=4;
-        Scanner sc=new Scanner(System.in);
-        for(int  i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                System.out.println("Enter the element: ");
+public class two_Dimensional_AL{
 
+    public static void print2D_Arr(ArrayList<ArrayList<Integer>> arr){
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr.get(i).size();j++){
+                System.out.print(arr.get(i).get(j)+" ");
             }
+            System.out.println();
         }
+    }
+    public static void main(String args[]){
+        ArrayList<ArrayList<Integer>> arr1=new ArrayList<>();
+        ArrayList<Integer> arr2=new ArrayList<>();
+        int row=3;
+        int col=4;
+        
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print("Enter the element: ");
+                arr2.add(sc.nextInt());
+            }
+            arr1.add(arr2);
+        }
+
+        print2D_Arr(arr1);
     }
 }
