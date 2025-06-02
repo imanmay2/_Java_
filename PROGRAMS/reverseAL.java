48c6f1913fb642412185a25935db0ff805f1c649
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-02 15:26:10 +0530

    ArrayList basics

---
+++ PROGRAMS/reverseAL.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class reverseAL {
+
+    public static void printArr(ArrayList<Integer> arr){
+        for(int i=0;i<arr.size();i++){
+            System.out.print(arr.get(i)+" ");
+        }
+    }
+    public  static void main(String args[]){
+        ArrayList<Integer> arr=new ArrayList<>();
+        System.out.println("Enter the size of  the array: ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.print("Enter the element: ");
+            int ele=sc.nextInt();
+            arr.add(ele);
+        }
+        printArr(arr);
+    }
+}
