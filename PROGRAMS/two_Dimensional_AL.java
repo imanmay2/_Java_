ddd7e3b99fdea5b259427deb5c34712aae304d95
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-02 15:58:41 +0530

    2D arraylist yet to be done

---
+++ PROGRAMS/two_Dimensional_AL.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class two_Dimensional_AL {
+    public static void main(String args[]){
+        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
+        int rows=3;
+        int cols=4;
+        Scanner sc=new Scanner(System.in);
+        for(int  i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                System.out.println("Enter the element: ");
+
+            }
+        }
+    }
+}
