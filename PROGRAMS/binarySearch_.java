8a517f79522a5ef9bccabffe042af2fa768da9a8
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-02 11:52:28 +0530

    yet to be done

--- PROGRAMS/binarySearch_.java
+++ PROGRAMS/binarySearch_.java
@@ -29,6 +29,10 @@ public class binarySearch_{
         }
         return mid;
     }
+
+    public static int binarySearchReursion(int arr[],int target,int si,int ei){
+        
+    }
     public static void main(String args[]){
         int arr[]={4,5,6,7,0,1,2};
         System.out.println(binarySearch(arr, 7, 0, arr.length-1));
