a6cb09db1c2f010b0475a6bea5067f57ba44674f
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-02 11:51:08 +0530

    binary search code working

---
+++ PROGRAMS/binarySearch_.java
@@ -0,0 +1,36 @@
+public class binarySearch_{
+
+    public static int binarySearch(int arr[],int target,int si,int ei){
+        ///base case
+        if(si>ei){
+            return -1;
+        }
+
+
+        int mid=si+(ei-si)/2;
+
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[si]<=arr[mid]){
+            //Line L1
+            if(arr[si]<=target && target<=arr[mid]){
+                return binarySearch(arr, target, si, mid-1);
+            } else{
+                return binarySearch(arr, target, mid+1, ei);
+            }
+        } else if(arr[mid]<=arr[ei]){
+            //Line L2
+            if(arr[mid]<=target && target<=arr[ei]){
+                return binarySearch(arr, target, mid+1, ei);
+            } else{
+                return  binarySearch(arr, target, si, mid-1);
+            }
+        }
+        return mid;
+    }
+    public static void main(String args[]){
+        int arr[]={4,5,6,7,0,1,2};
+        System.out.println(binarySearch(arr, 7, 0, arr.length-1));
+    }
+}
