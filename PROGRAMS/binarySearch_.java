a72a58334a278f62aef8ba69919f710bba8c1593
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-02 14:05:26 +0530

    divide and conquor  concpet covered

--- PROGRAMS/binarySearch_.java
+++ PROGRAMS/binarySearch_.java
@@ -31,10 +31,24 @@ public class binarySearch_{
     }
 
     public static int binarySearchReursion(int arr[],int target,int si,int ei){
-        
+        if(si>ei){
+            return -1;
+        }
+
+        int mid=si+(ei-si)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+
+
+        else if(arr[mid]>target){
+            return binarySearchReursion(arr, target, si, mid-1);
+        } else{
+            return binarySearchReursion(arr, target, mid+1, ei);
+        }
     }
     public static void main(String args[]){
-        int arr[]={4,5,6,7,0,1,2};
-        System.out.println(binarySearch(arr, 7, 0, arr.length-1));
+        int arr[]={0,1,2,4,5,6,7};
+        System.out.println(binarySearchReursion(arr, 6, 0, arr.length-1));
     }
 }
