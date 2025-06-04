3fbd5ce5c09bac4f6beaba182705c60b4d33b658
manmay2 <imanmay2@gmail.com>
2025-06-04 21:10:16 +0530

    updated

--- PROGRAMS/linked_list.java
+++ PROGRAMS/linked_list.java
@@ -1,252 +1,152 @@
-public class linked_list{
-    public static class Node{
+public class linked_list {
+     public static class Node{
         int data;
         Node next;
+
         public Node(int data){
             this.data=data;
             this.next=null;
         }
-    }
+     }
 
-    public static Node head;
-    public static Node tail;
-    public static int size;
-    public static void addFirst(int data){      
+     public static Node head;
+     public static Node tail;
+     public static int size;
+
+     public void addFirst(int data){
         Node newNode=new Node(data);
         if(head==null){
             head=tail=newNode;
-            size++;
             return;
         }
         newNode.next=head;
         head=newNode;
-        size++;
-    }
+     }
 
-    public static void addLast(int data){
+     public void addLast(int data){
         Node newNode=new Node(data);
         if(head==null){
-            head=tail=newNode;
-            size++;
+            head=tail=newNode;   //it will create a singleNode.
             return;
         }
         tail.next=newNode;
         tail=newNode;
-        size++;
-    }
+     }
 
-    public static boolean isEmpty(){
-        if(head==null){
-            return true;
-        }
-        return false;
-    }
-
-    public static void print_LL(){
+     public void print_LL(){
         Node temp=head;
         while(temp!=null){
-            System.out.print(temp.data+"--->");
+            System.out.print(temp.data+"-->");
             temp=temp.next;
         }
-        System.out.print("Null");
-    }
+        System.out.println("null");
+     }
+
+     public void addMiddle(int idx,int data){
+      Node newNode=new Node(data);
+      Node temp=head;
+      int i=0;
+      while(i<idx-1){
+         temp=temp.next;
+         i++;
+      }
+      //logic for adding the node in the index.
+      newNode.next=temp.next;
+      temp.next=newNode;
+     }
 
 
-    public static void add_middle(int idx,int data){
-        Node newNode=new Node(data);
-        if(head==null){
-            head=tail=newNode;
-            return;
-        }
-        else if(idx==0){
-            addFirst(data);
-        }
-        int i=0;
-        Node temp=head;
-        while(i<idx-1){
-            temp=temp.next;
-            i++;
-        }
-        newNode.next=temp.next;
-        temp.next=newNode;
-        size++;
-    }
+
+     public void size(){
+      Node temp=head;
+      while(temp!=null){
+         size++;
+         temp=temp.next;
+      }
+      System.out.println("Size of the LinkedList is: "+size);
+     }
+
+     public void removeFirst(){
+      size();
+      if(size==0){
+         System.out.println("LinkedList is empty already!");
+         return;
+      }
+      if(size==1){
+         head=tail=null;
+         return;
+      }
+      head=head.next;
+     }
+
+     public void reverse(){
+      Node next;
+      Node curr=tail=head;
+      Node prev=null;
+      while(curr!=null){
+         next=curr.next;
+         curr.next=prev;
+         prev=curr;
+         curr=next;
+      }
+      head=prev;
+     }
+
+     public void removeNthNodeEnd(int n){
+      Node temp=head;
+      int sz=0;
+      while(temp!=null){
+         temp=temp.next;
+         sz++;
+      }
+      
+      if(n==sz){
+         head=head.next;
+         return;
+      }
+
+      int i=0;
+      Node t=head;
+      int idx=sz-n;
+      while(i<idx-1){
+         t=t.next;
+         i++;
+      }
+      t.next=t.next.next;
+      return;
+     }
+
+     public void recursiveSearch(int key,Node temp,int idx){
+      if(temp==null){
+         System.out.println("Key not found in the list.");
+         return;
+      }
+      if(key==temp.data){
+         System.out.println("Found at index:"+idx);
+         return;
+      }
+      recursiveSearch(key, temp.next, idx+1);
+     }
 
 
-    public static void Remove_First(){
-        if(size==0){
-            System.out.println("LinkedList is Empty.");
-            return;
-        }
-        else if(size==1){
-            head=tail=null;
-            size--;
-            return;
-        }
-        head=head.next;
-        size--;
-    }
-    public static void Reverse_LL(){
-        Node next;
-        Node prev=null;
-        Node curr=tail=head;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-        head=prev;
-    }
-
-    
-
-
-    public static void Remove_Nth_Node_from_end(int ele_){
-        if(ele_>size){
-            System.out.println("Sorry, Index out of Bound!!!");
-            return;
-        }
-        int idx=size-ele_;
-        int i=0;
-        Node temp=head;
-        while(i<idx-1){
-            i++;
-            temp=temp.next;
-        }
-
-        temp.next=temp.next.next;
-        size--;
-    }
-
-    public static  Node find_midNode(){
-        Node slow=head;
-        Node fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-
-    public static boolean check_Pallindrome(){
-        if(head==null || head.next==null){
-            return true;
-        }
-        Node midNode=find_midNode();
-
-        //Reverse the 2nd half of the LinkedList.
-        Node prev=null;
-        Node curr=midNode;
-        Node next;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-        Node right=prev;
-        Node left=head;
-
-
-        //checking the 1st and 2nd half.
-        while(right!=null){
-            if(left.data!=right.data){
-                return false;
-            }
-            left=left.next;
-            right=right.next;
-        }
-        return true;
-    }
-    public static  boolean detect_cycle(){
-        Node slow=head;
-        Node fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static void remove_Cycle(){
-        boolean f=false;
-        Node slow=head;
-        Node fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast){
-                f=true;
-                break;
-            }
-        }
-        if(f==true){
-            Node prev=null;
-            slow=head;
-            while(fast!=slow){
-                prev=fast;
-                slow=slow.next;
-                fast=fast.next;
-            }
-            prev.next=null;
-            System.out.println("Removed the cycle in the linkedlist.");
-        }
-        else{
-            System.out.println("LinkedList is not in cycle.");
-        }
-    }
-
-
-    public static void zig_zag(){
-        Node midNode=find_midNode();
-
-
-        //reversing the 2nd half linkedlist.
-        Node prev=null;
-        Node curr=midNode;
-        Node next;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-        Node LH=head;
-        Node RH=prev;
-        Node nextL,nextR;
-        while(LH!=null && RH!=null){
-            nextL=LH.next;
-            nextR=RH.next;
-            RH.next=nextL;
-            RH=nextR;
-            LH=nextL;
-        }
-    }
-    
-
-    
-    public static void main(String args[]){
-        // head=new Node(1);
-        // head.next=new Node(2);
-        // head.next.next=new Node(3);
-        // head.next.next.next=head.next;
-
-
+      public static void main(String args[]){
         linked_list ll=new linked_list();
         ll.addLast(1);
         ll.addLast(2);
-        ll.addLast(3);
         ll.addLast(4);
-        // ll.print_LL();
-        // System.out.println(ll.check_Pallindrome());
-        // System.out.println();
-        // System.out.println();
-        // System.out.println("The length of the linked list is : "+ll.size);
-        ll.zig_zag();
+        ll.addLast(5);
         ll.print_LL();
-    }
+
+        
+      //   ll.reverse();
+      //   ll.print_LL();
+
+      //   ll.addMiddle(2,3);
+      //   ll.print_LL();
+      //   ll.removeFirst();
+      //   ll.print_LL();
+      //   ll.removeFirst();
+      //   ll.print_LL();
+        ll.recursiveSearch(4, head, 0);
+     }
 }
