3882c89df9b31114406662a560bc09c3a8ebb842
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-09 15:48:06 +0530

    Circular_Queue

--- PROGRAMS/tempCodeRunnerFile.java
+++ PROGRAMS/tempCodeRunnerFile.java
@@ -1,7 +1,4 @@
-Student(Student s1){
-        this.name=s1.name;
-        this.roll=s1.roll;
-        for(int i=0;i<marks.length;i++){
-            this.marks[i]=s1.marks[i];
-        }
-    }
+ // while(!q1.isEmpty()){
+        //     // q1.peek();
+        //     q1.dequeue();
+        // }
