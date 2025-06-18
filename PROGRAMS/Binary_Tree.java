14be4c2c040fa8792d2c9bf6961fdf79c48a3c21
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-18 15:56:13 +0530

    Level Order Traversal remaining.

--- PROGRAMS/Binary_Tree.java
+++ PROGRAMS/Binary_Tree.java
@@ -61,6 +61,8 @@ public class Binary_Tree {
         }
 
 
+
+
        public int sumNodes(Node root){
         if(root==null){
             return 0;
@@ -90,6 +92,7 @@ public class Binary_Tree {
         int right=heightNode(root.right);
         return Math.max(left, right)+1;
        }
+       
     }
     public static void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
