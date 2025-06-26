c9b4c91035eda36473f2a10aa3a6abc0bf5b82dc
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-26 10:16:09 +0530

    Assignment Questuion done

--- Assignments/Array_2.java
+++ Assignments/Array_2.java
@@ -1,10 +1,27 @@
+import java.util.*;
 
-import java.util.Scanner;
-public class Array_2 {
+public class Array_2{
+
+    public static boolean question1(int arr[]){
+        Arrays.sort(arr); 
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number:  ");
-        int num=sc.nextInt();
-        System.out.println("Number you have entered is : "+num);
+        System.out.println("Enter the range of the array: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.print("Enter the element: ");
+            arr[i]=sc.nextInt();
+        }
+
+
+        System.out.println(question1(arr));
     }
 }
