800d643a27f71421e5cd260182d7b24c17c612b7
manmay2 <imanmay2@gmail.com>
2025-07-27 13:37:24 +0530

    CN LAB1 done

--- PROGRAMS/prime_number_24BAI1316.java
+++ PROGRAMS/prime_number_24BAI1316.java
@@ -3,7 +3,7 @@ public class prime_number_24BAI1316 {
 
     public static boolean checkPrime(int n){
         int ct=0;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<=n;i++){
             if(n%i==0){
                 ct++;
             }
