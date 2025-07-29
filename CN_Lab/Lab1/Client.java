5c14f13ac8957fa8dfac260333a61640ebd973bc
manmay2 <imanmay2@gmail.com>
2025-07-29 23:02:26 +0530

    Finally donw with Server and Client Connection in Java

---
+++ CN_Lab/Lab1/Client.java
@@ -0,0 +1,54 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket; //client will send connection to Server via connect method.
+import java.net.UnknownHostException;
+
+public class client {
+
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream output = null;
+
+    public client(String address,int port){
+        try{
+            socket=new Socket(address,port);
+            System.out.println("Client Connected...");
+            input =new DataInputStream(System.in);  
+            output=new DataOutputStream(socket.getOutputStream()); 
+        } catch(UnknownHostException u){
+            System.out.println(u);
+            return;
+        } catch(IOException i){
+            System.out.println(i);
+            return;
+        }
+
+        String line=new String(); //creates a empty String.
+        while(!line.equals("end")){
+            try{
+                line=input.readLine(); // accepts data from the keyboard.
+                output.writeUTF(line); // sends the data to the Server.
+            } catch(UnknownHostException u){
+                System.out.println(u);
+                return;
+            } catch(IOException i){
+                System.out.println(i);
+                return;
+            }
+        }
+
+            try{
+                input.close();
+                output.close();
+                socket.close();
+            } catch(IOException i){
+                System.out.println(i);
+                return;
+            }
+    }
+
+    public static void main(String args[]) {
+        client cli=new client("127.0.0.1",5001);
+    }
+}
