1bdea8e148f113f3744da05d94f3d2763d97c5b5
manmay2 <imanmay2@gmail.com>
2025-08-02 09:17:01 +0530

    insertionSort and selectionSort done

--- PROGRAMS/bubbleSort_.java
+++ PROGRAMS/bubbleSort_.java
@@ -2,15 +2,42 @@ import java.util.Scanner;
 
 public class bubbleSort_ {
 
-    public static void bubbleSort(int arr[]){
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+    public static void bubbleSort(int arr[],int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }  
+        }
+    }
+
+    public static void insertionSort(int arr[],int n){
+        for(int i=1;i<n;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+    }
+
+    public static void selectionSort(int arr[],int n){
+        for(int i=0;i<n-1;i++){
+            int minpos=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[minpos]>arr[j]){
+                    minpos=j;
                 }
             }
+
+            int temp=arr[minpos];
+            arr[minpos]=arr[i];
+            arr[i]=temp;
         }
     }
 
@@ -28,8 +55,7 @@ public class bubbleSort_ {
         }
         
 
-        bubbleSort(arr);
-
+        selectionSort(arr,n);
         printArr(arr);
     }
 }
