d5f34caacaa8328c9a90fcc35ebf3bd1dbb8623c
manmay2 <imanmay2@gmail.com>
2025-08-04 01:00:20 +0530

    Assignment 2 Yet to be done

---
+++ CN_Lab/Assignment_Task2/client.java
@@ -0,0 +1,7 @@
+// Develop a client - server program to transfer the text message from server to client  
+// and display the same in client till the user types "End" message in server side using Java.
+
+
+public class client {
+    
+}
