94d573958db05bcd9397925f3986b895e2532adb
manmay2 <imanmay2@gmail.com>
2025-08-10 00:11:35 +0530

    updated

--- CN_Lab/Bi_directional_/Client.java
+++ CN_Lab/Bi_directional_/Client.java
@@ -5,22 +5,20 @@ import java.util.*;
 public class Client {
 
     // Reg No : 24BAI1316, Name : Manmay Chakraborty
-    
+
     public static void main(String[] args) {
         try (Socket socket = new Socket("localhost", 5001)) {
-             // Sample string to send
-           // String str = "Hello from client!";
-           // to read string from the user
-              Scanner scanner = new Scanner(System.in);
+            // Sample string to send
+            // String str = "Hello from client!";
+            // to read string from the user
+            Scanner scanner = new Scanner(System.in);
             // To send string to server
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             // To read response from server
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                     
-
-        System.out.print("Enter a string: ");
-        String str = scanner.nextLine(); 
+            System.out.print("Enter a string: ");
+            String str = scanner.nextLine();
             System.out.println("Sending string to server: " + str);
             out.println(str);
 
