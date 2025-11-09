05b041e406c612470a6ddf1afee9f65f12f71096
manmay2 <imanmay2@gmail.com>
2025-11-09 19:35:30 +0530

    Mutliclient and server UDP program.

--- CN_Lab/FAT/UDPServer.java
+++ CN_Lab/FAT/UDPServer.java
@@ -3,23 +3,24 @@ import java.net.*;
 
 public class UDPServer {
     public static void main(String args[]) {
-        
+
         try {
             DatagramSocket socket = new DatagramSocket(5000);
-        System.out.println("Waiting for client");
+            System.out.println("Waiting for client");
             while (true) {
                 DatagramPacket rp = new DatagramPacket(new byte[1024], 1024);
                 socket.receive(rp);
 
-                String str=new String(rp.getData(),0,rp.getLength());
-                System.out.println("Server : received : "+str);
+                String str = new String(rp.getData(), 0, rp.getLength());
+                System.out.println("Client : " + rp.getAddress() + "Port : " + rp.getPort() + "Data recieved : " + str);
 
-                //server sending
-                String s="MSG RECEIVED BY SERVER.";
-                DatagramPacket sp=new DatagramPacket(s.getBytes(),s.length(),rp.getAddress(),rp.getPort());
+                // server sending
+                String s = str.toUpperCase();
+                DatagramPacket sp = new DatagramPacket(s.getBytes(), s.length(), rp.getAddress(), rp.getPort());
                 socket.send(sp);
+
             }
-           
+
         } catch (Exception e) {
             e.printStackTrace();
         }
