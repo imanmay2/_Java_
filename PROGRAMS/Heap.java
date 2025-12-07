7b58e50ab658ddaafb85238db9ef07b4653efe4d
manmay2 <imanmay2@gmail.com>
2025-12-07 22:33:55 +0530

    Remove Heap code done

--- PROGRAMS/Heap.java
+++ PROGRAMS/Heap.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Heap{
 
     //inserting into the heap(min heap).
-    public static ArrayList<Integer> insertHeap(ArrayList<Integer> list,int ele){
+    public static void insertHeap(ArrayList<Integer> list,int ele){
         list.add(ele);
         int i=list.size()-1;
         while(i>0){
@@ -16,7 +16,6 @@ public class Heap{
             }
             i=pIdx;
         }
-        return list;
     }
 
     //peek element from the heap(min heap).
@@ -24,18 +23,72 @@ public class Heap{
         return list.get(0);
     }
 
+    
+    //printing the arrayList
     public static void printList(ArrayList<Integer> list){
         System.out.println("Elements in the list are : ");
         for(int i=0;i<list.size();i++){
             System.out.print(list.get(i)+" ");
         } System.out.println();
     }
-    public static void main(String args[]){
-        ArrayList<Integer> list=new ArrayList<>(Arrays.asList(2,3,4,5,10));
-        ArrayList<Integer> list1=insertHeap(list,1);
-        printList(list1);
 
-        System.out.println("Peek element from the heap is : ");
-        System.out.println(peekHeap(list1));
+
+    public static void heapify(int i,ArrayList<Integer> list){
+        int leftIdx=2*i+1;
+        int rightIdx=2*i+2;
+        int minIdx=i;
+
+        if(leftIdx<list.size() && list.get(minIdx)>list.get(leftIdx)){
+            minIdx=leftIdx;
+        }
+
+        if(rightIdx<list.size() && list.get(minIdx)>list.get(rightIdx)){
+            minIdx=rightIdx;
+        }
+
+        if(i==minIdx){
+            return;
+        }
+
+        //swapping
+        int temp=list.get(i);
+        list.set(i,list.get(minIdx));
+        list.set(minIdx,temp);
+
+        heapify(minIdx,list);
+        
+
+    }
+    //remove element from the heap.
+    public static int removeHeap(ArrayList<Integer> list){
+
+        //step1 : swapping the first and the last node
+        int t=list.get(0);
+        list.set(0,list.get(list.size()-1));
+        list.set(list.size()-1,t);
+
+        //step 2 : remove the last node
+        int data=list.get(list.size()-1);
+        list.remove(list.size()-1);
+
+        //step:3 heapify / fix the heap
+        heapify(0,list);
+
+        return data;
+    }
+    public static void main(String args[]){
+        ArrayList<Integer> list=new ArrayList<>(Arrays.asList(2,3,4,5,10,6));
+        // insertHeap(list,1);
+        // printList(list);
+
+        // System.out.println("Peek element from the heap is : ");
+        // System.out.println(peekHeap(list));
+
+        // System.out.println("Element removed from the Heap is : "+removeHeap(list));
+        
+        // printList(list);
+        System.out.println("Element removed from the Heap is : "+removeHeap(list));
+        
+        printList(list);
     }
 }
