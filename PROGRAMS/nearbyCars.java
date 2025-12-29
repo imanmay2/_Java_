1168d9b4d1c10ead8251497a36121932dcc4262e
manmay2 <imanmay2@gmail.com>
2025-12-29 17:15:29 +0530

    Near By Cars -> Priority Queue.

--- PROGRAMS/nearbyCars.java
+++ PROGRAMS/nearbyCars.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class nearbyCars{
 
-    public class Points implements Comparable<Points>{
+    public static class Points implements Comparable<Points>{
         int x,y,dist;
 
         public Points(int x,int y,int dist){
@@ -15,7 +15,7 @@ public class nearbyCars{
         }
 
         @Override
-        public void compareTo(Points p1){
+        public int compareTo(Points p1){
             return this.dist-p1.dist;
         }
     }
@@ -25,18 +25,23 @@ public class nearbyCars{
     }
 
     public static void main(String args[]){
-        PriorityQueue pq=new PriorityQueue<>();
-        int arr={{3,3},{5,-1},{-2,4}};
+        PriorityQueue<Points> pq=new PriorityQueue<>();
+        int arr[][]={{3,3},{5,-1},{-2,4}};
         int k=2;
         for(int i=0;i<arr.length;i++){
             int dist=calcDist(arr[i][0],arr[i][1]);
             pq.add(new Points(arr[i][0],arr[i][1],dist));
         }
 
-
-        for(int i=0;i<k;i++){
-            System.out.printf("%d , %d",pq[i].x,pq[i].y);
-
+        System.out.println("Nearest Cars present : ");
+        int ct=0;
+        while(!pq.isEmpty()){
+            Points p1=pq.remove();
+            System.out.println(p1.x+" , "+p1.y);
+            ct++;
+            if(ct==k){
+                break;
+            }
         }
     }
 }
