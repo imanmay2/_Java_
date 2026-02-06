eab34fab9615da953637fd0d83730939a4fb6471
manmay2 <imanmay2@gmail.com>
2026-02-06 14:01:36 +0530

    connected graph

---
+++ programs_DSA/Graph_2.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+public class Graph_2 {
+
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    public static void dfs(ArrayList<Edge> graph[]){
+        boolean vis[]=new boolean[graph.length];
+        for(int i=0;i<graph.length;i++){
+            vis[i]=false;
+        }
+        for(int i=0;i<graph.length;i++){
+            if(!vis[i]){
+                dfsUtil(graph,i,vis);
+            }
+        }
+    }
+
+    public static void dfsUtil(ArrayList<Edge> graph[],int curr,boolean vis[]){
+        System.out.print(curr+" ");
+        vis[curr]=true;
+        
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e=graph[curr].get(i);
+            if(!vis[e.dest]){
+                dfsUtil(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int V = 6;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // adding manually for the 2 unconnected graph.
+        // Component 1
+        graph[0].add(new Edge(0, 1));
+        graph[1].add(new Edge(1, 0));
+
+        graph[1].add(new Edge(1, 2));
+        graph[2].add(new Edge(2, 1));
+
+        // Component 2
+        graph[3].add(new Edge(3, 4));
+        graph[4].add(new Edge(4, 3));
+
+        graph[4].add(new Edge(4, 5));
+        graph[5].add(new Edge(5, 4));
+
+
+        dfs(graph);
+    }
+}
