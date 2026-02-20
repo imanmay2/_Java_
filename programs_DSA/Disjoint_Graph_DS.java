07768c01b4b40f59f85fbc59fda484649f531487
manmay2 <imanmay2@gmail.com>
2026-02-20 14:14:47 +0530

    Disjoint_Graph_DS done

---
+++ programs_DSA/Disjoint_Graph_DS.java
@@ -0,0 +1,46 @@
+public class Disjoint_Graph_DS {
+    static int n = 8;
+    static int par[] = new int[n];
+    static int rank[] = new int[n];
+
+    public static void init() {
+        for (int i = 0; i < par.length; i++) {
+            par[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public static int find(int x) {
+        if (x == par[x]) {
+            return x;
+        }
+
+        return par[x];
+    }
+
+    public static void union(int a,int b){
+        int parA=find(a);
+        int parB=find(b);
+        if(rank[parA]==rank[parB]){
+            par[parB]=parA;
+            rank[parA]++;
+        }else if(rank[parA]>rank[parB]){
+            par[parB]=parA;
+
+        }else{
+            par[parA]=parB;
+        }
+    }
+
+    public static void main(String args[]) {
+        init(); //initialization function. 
+        System.out.println(find(3));
+        union(1, 3);
+        System.out.println(find(3));
+        union(2, 4);
+        union(3, 6);
+        union(1, 4);
+        System.out.println(find(3));
+        union(1, 5);
+    }
+}
