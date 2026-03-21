0bd89002ac0666461e8e469c62fc84e08ec37b91
manmay2 <imanmay2@gmail.com>
2026-03-21 22:47:29 +0530

    unbounded knapSack using tabulation done

--- programs_DSA/DP_part2.java
+++ programs_DSA/DP_part2.java
@@ -7,7 +7,6 @@ public class DP_part2{
             //valid case
             int ans1=val[n-1]+knapSack_Recursion(val, wt, n-1, w-wt[n-1]);
             int ans2=knapSack_Recursion(val, wt, n-1, w);
-
             return Math.max(ans1,ans2);
         }else{
             //invalid case
@@ -81,8 +80,8 @@ public class DP_part2{
     public static boolean targetSumTabulation(int arr[],int targetSum){
         boolean dp[][]=new boolean[arr.length+1][targetSum+1];
         init_TargetSumDP(dp);
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[i].length;j++){
+        for(int i=1;i<arr.length+1;i++){
+            for(int j=1;j<targetSum+1;j++){
                 int v=arr[i-1];
                 if(v<=j && dp[i-1][j-v]){
                     dp[i][j]=true;
@@ -93,11 +92,45 @@ public class DP_part2{
         } return dp[arr.length][targetSum];
     }
 
+    public static void init_unboundedKnapSack(int dp[][]){
+        //init 
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+        }
+
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=0;
+        }
+    }
+
+    // unbounded knapSack
+    public static int unbounded_knap_sack(int val[],int wt[],int W){
+        int dp[][]=new int[val.length+1][W+1];
+
+        //init
+        init_unboundedKnapSack(dp);
+        
+        for(int i=1;i<val.length+1;i++){
+            for(int j=1;j<W+1;j++){
+                int v=val[i-1];
+                int w=wt[i-1];
+                if(w<=j){
+                    int ans1=v+dp[i][j-w];
+                    int ans2=dp[i-1][j];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        } return dp[val.length][W];
+    }
+
+
     
     public static void main(String args[]){
-        // int val[]={15,14,10,45,30};
-        // int wt[]={2,5,1,3,4};
-        // int w=7;
+        int val[]={15,14,10,45,30};
+        int wt[]={2,5,1,3,4};
+        int w=7;
         // System.out.println(knapSack_Recursion(val, wt, val.length, 7));
         // int dp[][]=new int[val.length+1][w+1];
         // init(dp);
@@ -105,7 +138,9 @@ public class DP_part2{
 
         // System.out.println("Tabulation Format : "+knapSack_Tab(val, wt, 7));
 
-        int arr[]={4,2,7,1,3};
-        System.out.println("TargetSum : "+ targetSumTabulation(arr, 10));
+        // int arr[]={4,2,7,1,3};
+        // System.out.println("TargetSum : "+ targetSumTabulation(arr, 10));
+
+        System.out.println("Unbounded : "+unbounded_knap_sack(val, wt, w));
     }
 }
