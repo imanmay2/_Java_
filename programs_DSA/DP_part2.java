6157b4244d3b1db2b6e5e92dc8185abeebbf90a0
manmay2 <imanmay2@gmail.com>
2026-03-21 16:34:27 +0530

    kanp/sack tabulation done

--- programs_DSA/DP_part2.java
+++ programs_DSA/DP_part2.java
@@ -41,6 +41,38 @@ public class DP_part2{
             }
         }
     }
+
+
+    public static int knapSack_Tab(int val[],int wt[],int W){
+        int n=val.length;
+        int dp[][]=new int[n+1][W+1];
+
+        //initialization
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+        }
+
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=0;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                int w=wt[i-1];
+                int v=val[i-1];
+                if(w<=j){
+                    //valid condition
+                    int incProfit=v+dp[i-1][j-w];
+                    int excProfit=dp[i-1][j];
+                    dp[i][j]=Math.max(incProfit,excProfit);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        } return dp[n][W];
+    }
+
+    
     public static void main(String args[]){
         int val[]={15,14,10,45,30};
         int wt[]={2,5,1,3,4};
@@ -49,5 +81,7 @@ public class DP_part2{
         int dp[][]=new int[val.length+1][w+1];
         init(dp);
         System.out.println(knapSack_Memo(val, wt,val.length, w,dp));
+
+        System.out.println("Tabulation Format : "+knapSack_Tab(val, wt, 7));
     }
 }
