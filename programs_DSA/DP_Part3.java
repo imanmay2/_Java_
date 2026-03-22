617a6ed4726c9a4ff3ff70dd136bbd2d2336e023
manmay2 <imanmay2@gmail.com>
2026-03-22 17:52:53 +0530

    LCS_Tabulation

--- programs_DSA/DP_Part3.java
+++ programs_DSA/DP_Part3.java
@@ -44,6 +44,14 @@ public class DP_Part3 {
         int m=str2.length();
         int dp[][]=new int[n+1][m+1];
 
+        //initialization 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
                 if(str1.charAt(i-1)==str2.charAt(j-1)){
