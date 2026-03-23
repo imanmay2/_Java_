0846caf0c49b78be8662a625f3595caafa0b089d
manmay2 <imanmay2@gmail.com>
2026-03-24 00:01:53 +0530

    Edit Strings -- >insert,update,delete --> DP

--- programs_DSA/DP_part4.java
+++ programs_DSA/DP_part4.java
@@ -75,6 +75,34 @@ public class DP_part4 {
         return LCS(nums, arr2);
     }
 
+    public int minDistance(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        int dp[][]=new int[n+1][m+1];
+        
+        //init
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=j;
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    int insert=dp[i][j-1]+1;
+                    int remove=dp[i-1][j]+1;
+                    int update=dp[i-1][j-1]+1;
+                    dp[i][j]=Math.min(Math.min(insert,remove),update);
+                }
+            }
+        }return dp[n][m];
+    }
+
+
     public static void main(String args[]) {
         System.out.println("Longest Common SubString is. : " + LongestCommonSubString("ABCDE", "ABGCE"));
     }
