64e421f39743a00ea150f7bd2c7317f8021b1c70
manmay2 <imanmay2@gmail.com>
2026-05-05 23:09:18 +0530

    Backtracking on arrays.

---
+++ programs_DSA/BackTracking_.java
@@ -0,0 +1,27 @@
+public class BackTracking_ {
+
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }System.out.println();
+    }
+    public static void backtrackArray(int arr[],int i){
+        //base case
+        if(i==arr.length-1){
+            arr[i]=i+1;
+            System.out.println("Array first time");
+            printArr(arr);
+            arr[i]=arr[i]-2;
+            return;
+        }
+        arr[i]=i+1;
+        backtrackArray(arr, i+1);
+        arr[i]=arr[i]-2;
+    }
+    public static void main(String args[]){
+        int arr[]=new int[5];
+        backtrackArray(arr, 0);
+        System.out.println("Final Array : ");
+        printArr(arr);
+    }
+}
