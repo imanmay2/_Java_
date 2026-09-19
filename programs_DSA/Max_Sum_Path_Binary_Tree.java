96a55d9ab147e7a343d752d17219056e7ccf5f38
Manmay Chakraborty <imanmay2@gmail.com>
2026-09-20 00:18:35 +0530

    LC-124 ( Binary Tree MAxiumum Sum path Question)

--- programs_DSA/Max_Sum_Path_Binary_Tree.java
+++ programs_DSA/Max_Sum_Path_Binary_Tree.java
@@ -1,18 +1,6 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+
+
 class Solution {
     int maxSum=Integer.MIN_VALUE;
     public int sum(TreeNode root){
